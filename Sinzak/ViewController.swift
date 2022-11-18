4df537020e722bdbbc11af5204f339805d69db83
Doy Kim <kimdee.dev@gmail.com>
2022-11-19 00:11:06 +0900

    Initial Commit

---
+++ Sinzak/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sinzak
+//
+//  Created by Doy Kim on 2022/11/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
