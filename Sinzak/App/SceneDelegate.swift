132040bdeb8732fa426b67060587cbf83868c14f
Doy Kim <kimdee.dev@gmail.com>
2022-12-13 23:29:09 +0900

    [Feature] #13 SceneDelegate에서 루트뷰 설정

--- Sinzak/App/SceneDelegate.swift
+++ Sinzak/App/SceneDelegate.swift
@@ -15,7 +15,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        // 루트 뷰 변경
+        let vc = ConciergeVC()
+//        let rootViewController = UINavigationController(rootViewController: vc)
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = vc
+        self.window = window
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
