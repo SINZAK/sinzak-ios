be3537f3d0ef45819d32444d32e6959ebc08c182
Doy Kim <kimdee.dev@gmail.com>
2022-12-29 16:47:10 +0900

    [Setting] 서브뷰로 추가하는 메서드를 익스텐션으로 추가

---
+++ Sinzak/Global/Extension/UIView+UIStackView+Extension.swift
@@ -0,0 +1,22 @@
+//
+//  UIView+UIStackView+Extension.swift
+//  Sinzak
+//
+//  Created by Doy Kim on 2022/12/26.
+//
+
+import UIKit
+
+extension UIView {
+    /// 여러개의 뷰를 서브뷰로 한번에 추가합니다.
+    func addSubviews(_ views: UIView...) {
+        views.forEach { self.addSubview($0) }
+    }
+}
+
+extension UIStackView {
+    /// 여러개의 뷰를 스택뷰의 서브뷰로 한번에 추가합니다.
+    func addArangedSubviews(_ views: UIView...) {
+        for view in views { addArrangedSubview(view) }
+    }
+}
