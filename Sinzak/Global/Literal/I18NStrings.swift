224f7c229be46ab64bee348f0531421065f4e312
Doy Kim <kimdee.dev@gmail.com>
2022-12-29 17:31:30 +0900

    [Design] #18 회원가입 화면에 문자열 리터럴 추가, 다국어 설정

--- Sinzak/Global/Literal/I18NStrings.swift
+++ Sinzak/Global/Literal/I18NStrings.swift
@@ -8,7 +8,47 @@
 import Foundation
 
 struct I18NStrings {
-    // 로그인 화면
+    // - 공통
+    static let confirm = "confirm".localized
+    static let next = "next".localized
+    
+    // - 로그인 화면
     static let logoText = "logoText".localized
     static let startWithSnsLogin = "startWithSnsLogin".localized
+    
+    // - 회원가입 화면
+    // 서비스 이용동의
+    static let consentToUseOfService = "consentToUseOfService".localized
+    static let fullAgree = "fullAgree".localized
+    static let olderThanfourteenYears = "olderThanfourteenYears".localized
+    static let requiredTermsOfService = "requiredTermsOfService".localized
+    static let requiredPrivacyPolicy = "requiredPrivacyPolicy".localized
+    static let optionalMarketingInformation = "optionalMarketingInformation".localized
+    
+    // 이름 입력
+    static let pleaseEnterName = "pleaseEnterName".localized
+    static let nameValidationDescription = "nameValidationDescription".localized
+    
+    // 관심장르 선택
+    static let pleaseSelectGenreOfInterest = "pleaseSelectGenreOfInterest".localized
+    static let upToThreeCanBeSelected = "upToThreeCanBeSelected".localized
+    
+    // 대학교 선택
+    static let pleaseSelectUniversity = "pleaseSelectUniversity".localized
+    static let yearOfAdmission = "yearOfAdmission".localized
+    static let selectYearStudentNumber = "selectYearStudentNumber".localized
+    static let selectAcademicStatus = "selectAcademicStatus".localized
+    static let enrolled = "enrolled".localized
+    static let graduated = "graduated".localized
+    static let universityName = "universityName".localized
+    static let searchBySchoolName = "searchBySchoolName".localized
+    
+    // 대학교 인증
+    static let enrolledStudentAuth = "enrolledStudentAuth".localized
+    static let graduatedStudentAuth = "graduatedStudentAuth".localized
+    static let schoolWebMailAuth = "schoolWebMailAuth".localized
+    static let emailAuthDescription = "emailAuthDescription".localized
+    static let pleaseEnterSchoolEmail = "pleaseEnterSchoolEmail".localized
+    static let doNextTime = "doNextTime".localized
+    static let sendMail = "sendMail".localized
 }
