6d9e50844dfb62ab6ead229e0af3284bf4f0fc64
Doy Kim <kimdee.dev@gmail.com>
2023-01-06 13:09:10 +0900

    [Design] #18 회원가입-이름 화면에서 다음 버튼으로 장르화면 연결

--- Sinzak/Scene/Signup/VC/SignupNameVC.swift
+++ Sinzak/Scene/Signup/VC/SignupNameVC.swift
@@ -10,7 +10,6 @@ import UIKit
 final class SignupNameVC: SZVC {
     // MARK: - Properties
     let mainView = SignupNameView()
-    
     // MARK: - Lifecycle
     override func loadView() {
         view = mainView
@@ -18,8 +17,15 @@ final class SignupNameVC: SZVC {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
     // MARK: - Actions
-    
+    @objc
+    func nextButtonTapped(_ sender: UIButton) {
+        let vc = SignupGenreVC()
+        vc.modalPresentationStyle = .fullScreen
+        navigationController?.pushViewController(vc, animated: true)
+    }
     // MARK: - Helpers
+    override func configure() {
+        mainView.nextButton.addTarget(self, action: #selector(nextButtonTapped), for: .touchUpInside)
+    }
 }
