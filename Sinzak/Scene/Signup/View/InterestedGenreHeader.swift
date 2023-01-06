dff77427083bd5fdf3a9ba09ea467b75738de4e6
Doy Kim <kimdee.dev@gmail.com>
2023-01-06 13:07:28 +0900

    [Design] #18 장르 콜렉션뷰 헤더 구성

---
+++ Sinzak/Scene/Signup/View/InterestedGenreHeader.swift
@@ -0,0 +1,38 @@
+//
+//  InterestedGenreHeader.swift
+//  Sinzak
+//
+//  Created by Doy Kim on 2023/01/06.
+//
+
+import UIKit
+
+import Then
+import SnapKit
+
+final class InterestedGenreHeader: UICollectionReusableView {
+    let titleLabel = UILabel().then {
+        $0.font = .body_B
+        $0.textColor = CustomColor.black
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+        setConstraints()
+    }
+        
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupUI() {
+       addSubview(titleLabel)
+    }
+    func setConstraints() {
+        titleLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+        }
+    }
+    
+}
