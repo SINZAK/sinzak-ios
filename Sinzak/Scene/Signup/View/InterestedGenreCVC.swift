b578723fd7d2ba62e5109285b0bd539434513478
Doy Kim <kimdee.dev@gmail.com>
2023-01-06 13:07:11 +0900

    [Design] #18 장르 콜렉션뷰 셀 구성

---
+++ Sinzak/Scene/Signup/View/InterestedGenreCVC.swift
@@ -0,0 +1,74 @@
+//
+//  InterestedGenreCVC.swift
+//  Sinzak
+//
+//  Created by Doy Kim on 2023/01/06.
+//
+
+import UIKit
+
+import Then
+import SnapKit
+
+final class InterestedGenreCVC: UICollectionViewCell {
+    // MARK: Properties
+    let imageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFit
+    }
+    let textLabel = UILabel().then {
+        $0.font = .caption_B
+    }
+    let baseView = UIView().then {
+        $0.layer.cornerRadius = 15
+    }
+    // MARK: Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupUI()
+        setConstraints()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    // 셀 선택여부에 따라 디자인 변경
+    func designUI(isSelected: Bool = false) {
+        switch isSelected {
+        case true:
+            textLabel.textColor = CustomColor.white
+            baseView.backgroundColor = CustomColor.red
+            imageView.image = UIImage(named: "checkmark-white")
+        case false:
+            textLabel.textColor = CustomColor.black
+            baseView.backgroundColor = CustomColor.gray10
+            imageView.image = UIImage(named: "checkmark-black")
+        }
+    }
+    
+    func setupUI() {
+        backgroundColor = .clear
+        contentView.addSubviews(
+            baseView, imageView, textLabel
+        )
+    }
+    func setConstraints() {
+        imageView.snp.makeConstraints { make in
+            make.width.height.equalTo(20)
+            make.leading.equalToSuperview().inset(8)
+            make.centerY.equalToSuperview()
+            make.top.bottom.equalToSuperview().inset(8)
+        }
+        textLabel.snp.makeConstraints { make in
+            make.leading.equalTo(imageView.snp.trailing).offset(2)
+            make.centerY.equalTo(imageView)
+            make.trailing.equalToSuperview().inset(15)
+        }
+        baseView.snp.makeConstraints { make in
+            make.top.equalTo(imageView).offset(-8)
+            make.bottom.equalTo(imageView).offset(8)
+            make.leading.equalTo(imageView).offset(-8)
+            make.trailing.equalTo(textLabel).offset(15)
+        }
+        designUI()
+    }
+}
