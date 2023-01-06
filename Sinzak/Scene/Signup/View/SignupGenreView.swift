cd36035f868982152b7907dbff63f4a8ab322c9a
Doy Kim <kimdee.dev@gmail.com>
2023-01-06 13:07:50 +0900

    [Design] #18 회원가입-장르 뷰 구성

--- Sinzak/Scene/Signup/View/SignupGenreView.swift
+++ Sinzak/Scene/Signup/View/SignupGenreView.swift
@@ -12,6 +12,55 @@ import SnapKit
 
 final class SignupGenreView: SZView {
     // MARK: - Properties
-    
-    // MARK: - Design Helpers}
+    private let titleLabel = UILabel().then {
+        $0.text = I18NStrings.pleaseSelectGenreOfInterest
+        $0.font = .subtitle_B
+        $0.textColor = CustomColor.black
+        $0.numberOfLines = 0
+    }
+    private let descriptionLabel = UILabel().then {
+        $0.text = I18NStrings.upToThreeCanBeSelected
+        $0.font = .body_R
+        $0.textColor = CustomColor.gray60
+        $0.numberOfLines = 0
+    }
+    // 관심장르 목록 콜렉션 뷰
+    lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout()).then {
+        $0.backgroundColor = .clear
+        $0.bounces = false
+    }
+    // 다음 버튼
+    let nextButton = UIButton().then {
+        $0.setTitle(I18NStrings.next, for: .normal)
+        $0.titleLabel?.font = .body_B
+        $0.backgroundColor = CustomColor.gray10
+        $0.layer.cornerRadius = 30
+    }
+    // MARK: - Design Helpers
+    override func setView() {
+        addSubviews(
+            titleLabel, descriptionLabel, collectionView, nextButton
+        )
+    }
+    override func setLayout() {
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(safeAreaLayoutGuide).inset(15.87)
+            make.leading.equalToSuperview().inset(32.58)
+        }
+        descriptionLabel.snp.makeConstraints { make in
+            make.leading.equalTo(titleLabel)
+            make.top.equalTo(titleLabel.snp.bottom).offset(8.21)
+        }
+        collectionView.snp.makeConstraints { make in
+            make.leading.equalToSuperview().inset(37)
+            make.trailing.equalToSuperview().inset(20)
+            make.top.equalTo(descriptionLabel.snp.bottom).offset(18)
+            make.bottom.equalTo(nextButton.snp.top).offset(18)
+        }
+        nextButton.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(7.4)
+            make.height.equalTo(65)
+            make.bottom.equalTo(safeAreaLayoutGuide)
+        }
+    }
 }
