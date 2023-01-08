7daf69569f2868932630c7a4e3aff6103c5232ed
Doy Kim <kimdee.dev@gmail.com>
2023-01-08 23:25:25 +0900

    [Feature] #19 AppDelegate에 네이버로그인 SDK 설정

--- Sinzak/App/AppDelegate.swift
+++ Sinzak/App/AppDelegate.swift
@@ -7,13 +7,25 @@
 
 import UIKit
 import KakaoSDKCommon
+import NaverThirdPartyLogin
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // 카카오로그인
         KakaoSDK.initSDK(appKey: "kakaof4e54dad18c8af8a67dccb0176283616")
+        // 네이버로그인
+        application.registerForRemoteNotifications()
+        let instance = NaverThirdPartyLoginConnection.getSharedInstance()
+        instance?.isNaverAppOauthEnable = true //네이버앱 로그인 설정
+        instance?.isInAppOauthEnable = true //사파리 로그인 설정
+
+        instance?.serviceUrlScheme = "naverLoginSinzak" //URL Scheme
+        instance?.consumerKey = "DwXMEfKZq0tmkrsn6kLk" //클라이언트 아이디
+        instance?.consumerSecret = "2CAzvT18ok" //시크릿 아이디
+        instance?.appName = "신작" //앱이름
         return true
     }
 
@@ -31,4 +43,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+}
+
+// 네이버로그인 SDK 설정
+extension AppDelegate {
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+            NaverThirdPartyLoginConnection.getSharedInstance()?.application(app, open: url, options: options)
+            return true
+    }
 }
