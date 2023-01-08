0e8d64d2a4ad066d65883b6c73333dab641e0888
Doy Kim <kimdee.dev@gmail.com>
2023-01-08 22:07:51 +0900

    [Feature] #19  Kakao SDK 초기화 코드 추가

--- Sinzak/App/AppDelegate.swift
+++ Sinzak/App/AppDelegate.swift
@@ -6,13 +6,15 @@
 //
 
 import UIKit
+import KakaoSDKCommon
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        KakaoSDK.initSDK(appKey: "${NATIVE_APP_KEY}")
+        
         return true
     }
 
