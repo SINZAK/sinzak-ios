02b815bb3cd110a7ef8418e851cd0d7aadfaebb6
Doy Kim <kimdee.dev@gmail.com>
2023-01-24 17:37:55 +0900

    [Design] #11 알림 콜렉션 뷰 셀에서 여백 수정, 이미지 코너 래디우스, 텍스트 라인을 가변으로 적용

--- Sinzak/Scene/Notification/View/NotificationCVC.swift
+++ Sinzak/Scene/Notification/View/NotificationCVC.swift
@@ -13,19 +13,21 @@ final class NotificationCVC: UICollectionViewCell {
     // MARK: - Properties
     let thumbnailImageView = UIImageView().then {
         $0.layer.cornerRadius = 12
+        $0.clipsToBounds = true
+        $0.contentMode = .scaleAspectFill
         $0.image = UIImage(named: "notiThumb")
     }
     let titleLabel = UILabel().then {
-        $0.text = "활동 알림입니다"
+        $0.text = "sinzak 님이 작품 ‘환상'에 가격 제안을 했어요. [24,000원]"
         $0.font = .body_M
         $0.textColor = CustomColor.black
+        $0.numberOfLines = 0
     }
     let timeLabel = UILabel().then {
         $0.text = "방금 전"
         $0.font = .caption_R
         $0.textColor = CustomColor.gray80
     }
-    
     // MARK: - Init
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -50,13 +52,14 @@ final class NotificationCVC: UICollectionViewCell {
         }
         titleLabel.snp.makeConstraints { make in
             make.top.equalToSuperview().inset(16)
-            make.leading.equalTo(thumbnailImageView.snp.trailing).offset(7)
+            make.leading.equalTo(thumbnailImageView.snp.trailing).offset(8)
             make.trailing.lessThanOrEqualToSuperview().inset(16)
         }
         timeLabel.snp.makeConstraints { make in
             make.top.equalTo(titleLabel.snp.bottom).offset(5)
             make.leading.equalTo(titleLabel)
             make.trailing.lessThanOrEqualToSuperview().inset(16)
+            make.bottom.equalToSuperview().inset(17)
         }
     }
 }
