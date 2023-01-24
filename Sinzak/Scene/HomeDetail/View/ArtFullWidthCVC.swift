eb0501867b70b4c0c2feccb7756b67b4c3764d87
Doy Kim <kimdee.dev@gmail.com>
2023-01-24 14:30:02 +0900

    [Design] #12 홈에서 더보기를 클릭했을 때 넘어가는 작품리스트 디테일 View, VC, CVC 파일 구성

---
+++ Sinzak/Scene/HomeDetail/View/ArtFullWidthCVC.swift
@@ -0,0 +1,14 @@
+//
+//  ArtFullWidthCVC.swift
+//  Sinzak
+//
+//  Created by Doy Kim on 2023/01/24.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+final class ArtFullWidthCVC: UICollectionViewCell {
+    
+}
---
+++ Sinzak/Scene/HomeDetail/View/HomeDetailView.swift
@@ -0,0 +1,14 @@
+//
+//  HomeDetailView.swift
+//  Sinzak
+//
+//  Created by Doy Kim on 2023/01/24.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+final class HomeDetailView: SZView {
+    
+}
