2014edb6f9dc7b0bf87141c8dfe4fe7f277be4cf
Doy Kim <kimdee.dev@gmail.com>
2023-01-24 13:31:57 +0900

    [Design] #10 컴포지셔널 레이아웃을 적용할 하나의 콜렉션 뷰를 뷰에 추가

--- Sinzak/Scene/Home/View/HomeView.swift
+++ Sinzak/Scene/Home/View/HomeView.swift
@@ -9,12 +9,19 @@ import UIKit
 
 class HomeView: SZView {
     // MARK: - Properties
-    
+    lazy var homeCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout()).then {
+        $0.backgroundColor = .clear
+    }
     // MARK: - Design Helpers
     override func setView() {
-        
+        addSubviews(
+            homeCollectionView
+        )
     }
     override func setLayout() {
-        
+        homeCollectionView.snp.makeConstraints { make in
+            make.trailing.leading.bottom.equalToSuperview()
+            make.top.equalTo(safeAreaLayoutGuide)
+        }
     }
 }
