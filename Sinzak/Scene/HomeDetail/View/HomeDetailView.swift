5bf12522d8f9044dfaef70983e4a288f618e7067
Doy Kim <kimdee.dev@gmail.com>
2023-01-24 15:14:09 +0900

    [Design] #12 콜렉션 뷰 정의

--- Sinzak/Scene/HomeDetail/View/HomeDetailView.swift
+++ Sinzak/Scene/HomeDetail/View/HomeDetailView.swift
@@ -10,5 +10,20 @@ import SnapKit
 import Then
 
 final class HomeDetailView: SZView {
-    
+    // MARK: - Properties
+    lazy var detailCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout()).then {
+        $0.backgroundColor = .clear
+    }
+    // MARK: - Design Helpers
+    override func setView() {
+        addSubviews(
+            detailCollectionView
+        )
+    }
+    override func setLayout() {
+        detailCollectionView.snp.makeConstraints { make in
+            make.trailing.leading.bottom.equalToSuperview()
+            make.top.equalTo(safeAreaLayoutGuide)
+        }
+    }
 }
