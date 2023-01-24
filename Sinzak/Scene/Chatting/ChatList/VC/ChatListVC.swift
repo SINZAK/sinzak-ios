285bb1ef5daaa6f1e08ed9c0d6e11499caddc0a6
Doy Kim <kimdee.dev@gmail.com>
2023-01-24 22:40:54 +0900

    [Design] #26 탭바 항상 보여줄 수 있게 변경

--- Sinzak/Scene/Chatting/ChatList/VC/ChatListVC.swift
+++ Sinzak/Scene/Chatting/ChatList/VC/ChatListVC.swift
@@ -14,6 +14,13 @@ final class ChatListVC: SZVC {
     override func loadView() {
         view = mainView
     }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tabBarController?.tabBar.isHidden = false
+    }
     // MARK: - Helper
     override func setNavigationBar() {
         navigationItem.title = "채팅"
@@ -26,14 +33,23 @@ final class ChatListVC: SZVC {
     }
 }
 extension ChatListVC: UICollectionViewDelegate, UICollectionViewDataSource {
+    // 아이템 갯수
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 5
     }
+    // 셀 구성
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: ChatListCVC.self), for: indexPath) as? ChatListCVC else { return UICollectionViewCell() }
         cell.verifiedBadge.isHidden = indexPath.item % 3 < 1 ? true : false
         cell.chatCountBackground.isHidden = indexPath.item % 3 > 0 ? true : false
         return cell
+    }
+    // 셀 클릭했을 때
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let vc = ChatVC()
+        // vc에 대화정보 전달
+        // 소켓통신 열기
+        navigationController?.pushViewController(vc, animated: true)
     }
 }
 // 컴포지셔널 레이아웃
