e7f58562504a7e882b86d8628dec30ba85e0c6e4
Doy Kim <kimdee.dev@gmail.com>
2023-01-29 16:44:42 +0900

    [Design] #23 마켓화면의 필터링 부분을 넣을 헤더파일 구성

---
+++ Sinzak/Scene/Chatting/ChatList/View/MarketHeader.swift
@@ -0,0 +1,26 @@
+//
+//  MarketHeader.swift
+//  Sinzak
+//
+//  Created by Doy Kim on 2023/01/29.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class MarketHeader: UICollectionReusableView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+        setConstraints()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    func setupUI() {
+    }
+    func setConstraints() {
+    }
+
+}
