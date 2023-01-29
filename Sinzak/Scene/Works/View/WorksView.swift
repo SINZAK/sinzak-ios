8362926295b27b14a286a72d5fc547df28cd2d45
Doy Kim <kimdee.dev@gmail.com>
2023-01-29 20:50:08 +0900

    [Design] #24 외주화면 View 디자인 - 콜렉션 뷰, 글쓰기 버튼

--- Sinzak/Scene/Works/View/WorksView.swift
+++ Sinzak/Scene/Works/View/WorksView.swift
@@ -8,6 +8,31 @@
 import UIKit
 
 final class WorksView: SZView {
-    
-    
+    // MARK: - Properties
+    lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout()).then {
+        $0.backgroundColor = .clear
+    }
+    let writeButton = UIButton().then {
+        $0.clipsToBounds = true
+        $0.layer.cornerRadius = 31
+        $0.backgroundColor = CustomColor.red
+        $0.setImage(UIImage(named: "plus"), for: .normal)
+    }
+    // MARK: - Design Helpers
+    override func setView() {
+        addSubviews(
+            collectionView, writeButton
+        )
+    }
+    override func setLayout() {
+        collectionView.snp.makeConstraints { make in
+            make.trailing.leading.bottom.equalToSuperview()
+            make.top.equalTo(safeAreaLayoutGuide)
+        }
+        writeButton.snp.makeConstraints { make in
+            make.width.height.equalTo(62)
+            make.bottom.equalTo(safeAreaLayoutGuide).inset(7)
+            make.trailing.equalToSuperview().inset(11)
+        }
+    }
 }
