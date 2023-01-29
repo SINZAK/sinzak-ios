600dbec873442bc02a09e09364839804ad75cc25
Doy Kim <kimdee.dev@gmail.com>
2023-01-29 17:51:09 +0900

    [Design] #23 마켓 헤더에 필터와 정렬버튼 추가

--- Sinzak/Scene/Market/View/MarketHeader.swift
+++ Sinzak/Scene/Market/View/MarketHeader.swift
@@ -10,6 +10,21 @@ import SnapKit
 import Then
 
 class MarketHeader: UICollectionReusableView {
+
+    let viewOptionButton = UIButton().then {
+        $0.setImage(UIImage(named: "radiobtn-unchecked"), for: .normal)
+        $0.setTitle("판매중 작품만 보기", for: .normal)
+        $0.titleLabel?.font = .caption_M
+        $0.setTitleColor(CustomColor.gray80, for: .normal)
+        $0.tintColor = CustomColor.gray80
+    }
+    let alignButton = UIButton().then {
+        $0.setImage(UIImage(named: "align"), for: .normal)
+        $0.setTitle("신작추천순", for: .normal)
+        $0.titleLabel?.font = .caption_B
+        $0.setTitleColor(CustomColor.gray80, for: .normal)
+        $0.tintColor = CustomColor.gray80
+    }
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupUI()
@@ -19,8 +34,20 @@ class MarketHeader: UICollectionReusableView {
         fatalError("init(coder:) has not been implemented")
     }
     func setupUI() {
+        addSubviews(
+            viewOptionButton,
+            alignButton
+        )
     }
     func setConstraints() {
+        viewOptionButton.snp.makeConstraints { make in
+            make.height.equalTo(22)
+            make.leading.equalToSuperview()
+        }
+        alignButton.snp.makeConstraints { make in
+            make.trailing.equalToSuperview()
+            make.height.equalTo(22)
+        }
     }
 
 }
