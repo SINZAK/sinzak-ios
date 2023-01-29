14a61aaffa821362b3072df383dfafa73e9269a9
Doy Kim <kimdee.dev@gmail.com>
2023-01-29 16:42:39 +0900

    [Design] #23 기존 탭뷰 대신 콜렉션뷰 + 컴포지셔널 레이아웃으로 추가 (상단 태그, 하단 작품리스트)

--- Sinzak/Scene/Market/VC/MarketVC.swift
+++ Sinzak/Scene/Market/VC/MarketVC.swift
@@ -6,22 +6,32 @@
 //
 
 import UIKit
-import Tabman
-import Pageboy
 
-final class MarketVC: TabmanViewController {
+final class MarketVC: SZVC {
+    // MARK: - Properties
+    let mainView = MarketView()
+    enum SectionKind: Int {
+        case category = 0
+        case art
+    }
+    // MARK: - Init
+    override func loadView() {
+        view = mainView
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         setNavigationBar()
         configure()
     }
+    // MARK: - Actions
+    /// 검색화면으로 이동
     @objc func searchButtonTapped(_ sender: UIBarButtonItem) {
-        // 검색화면으로 이동
         let vc = SZVC()
         navigationController?.pushViewController(vc, animated: true)
     }
-    func setNavigationBar() {
-        navigationController?.navigationBar.tintColor = CustomColor.black
+    // MARK: - Helpers
+    override func setNavigationBar() {
+        super.setNavigationBar()
         navigationItem.title = "마켓"
         let search = UIBarButtonItem(
             image: UIImage(named: "search"),
@@ -30,7 +40,85 @@ final class MarketVC: TabmanViewController {
             action: #selector(searchButtonTapped))
         navigationItem.rightBarButtonItem = search
     }
-    func configure() {
+    override func configure() {
         view.backgroundColor = CustomColor.white
+        mainView.collectionView.delegate = self
+        mainView.collectionView.dataSource = self
+        mainView.collectionView.register(ArtCVC.self, forCellWithReuseIdentifier: String(describing: ArtCVC.self))
+        mainView.collectionView.register(CategoryTagCVC.self, forCellWithReuseIdentifier: String(describing: CategoryTagCVC.self))
+        mainView.collectionView.collectionViewLayout = setLayout()
+    }
+}
+extension MarketVC: UICollectionViewDelegate, UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return section == SectionKind.category.rawValue ? Category.allCases.count : 9
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if indexPath.section == SectionKind.category.rawValue {
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: CategoryTagCVC.self), for: indexPath)  as? CategoryTagCVC else { return UICollectionViewCell() }
+            cell.categoryLabel.text = Category.allCases[indexPath.item].text
+            if indexPath.item == 0 {
+                cell.setColor(kind: .selected)
+            }
+            return cell
+        } else {
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: ArtCVC.self), for: indexPath) as? ArtCVC else { return UICollectionViewCell() }
+            return cell
+        }
+    }
+}
+// 컴포지셔널 레이아웃
+extension MarketVC {
+    /// 컴포지셔널 레이아웃 세팅
+    func setLayout() -> UICollectionViewCompositionalLayout {
+        return UICollectionViewCompositionalLayout { (sectionNumber, _) -> NSCollectionLayoutSection? in
+        // 카테고리 경우
+            if sectionNumber == SectionKind.category.rawValue {
+                let itemSize = NSCollectionLayoutSize(
+                    widthDimension: .estimated(70),
+                    heightDimension: .estimated(32))
+                let item = NSCollectionLayoutItem(layoutSize: itemSize)
+                let groupSize = NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(2.0),
+                    heightDimension: .estimated(100))
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+                group.interItemSpacing = .fixed(10)
+                let section = NSCollectionLayoutSection(group: group)
+                section.contentInsets.top = 15
+                section.contentInsets.leading = 16
+                section.contentInsets.bottom = 15
+                section.interGroupSpacing = 0
+                section.orthogonalScrollingBehavior = .continuous
+                return section
+            } else { // 카테고리가 아닐 경우
+                let itemSize = NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(0.47),
+                    heightDimension: .fractionalHeight(1.0)
+                )
+                let groupSize = NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1.0),
+                    heightDimension: .estimated(240)
+                )
+                let item = NSCollectionLayoutItem(layoutSize: itemSize)
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+                group.interItemSpacing = .fixed(16)
+                let section = NSCollectionLayoutSection(group: group)
+                section.contentInsets.leading = 16
+                section.contentInsets.trailing = 16
+                section.contentInsets.bottom = 72
+                // section.interGroupSpacing = 16
+                // 헤더 설정
+                // let headerItemSize = NSCollectionLayoutSize(
+                // widthDimension: .fractionalWidth(1.0),
+                // heightDimension: .estimated(40))
+                // let headerItem = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerItemSize, elementKind: "header", alignment: .top)
+                // section.boundarySupplementaryItems = [headerItem]
+                // section.orthogonalScrollingBehavior = .continuous
+                 return section
+            }
+        }
     }
 }
