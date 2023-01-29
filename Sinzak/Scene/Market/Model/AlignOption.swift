9febaa3aed4a1f66942b69eb1d2a4e3abf9e07b5
Doy Kim <kimdee.dev@gmail.com>
2023-01-29 17:01:09 +0900

    [Design] #23 정렬옵션 텍스트 및 설명 텍스트를 열거형 케이스와 computed property 로 정의

---
+++ Sinzak/Scene/Market/Model/AlignOption.swift
@@ -0,0 +1,41 @@
+//
+//  AlignOption.swift
+//  Sinzak
+//
+//  Created by Doy Kim on 2023/01/29.
+//
+
+import Foundation
+
+enum AlignOption: CaseIterable {
+    case recommend
+    case popular
+    case recent
+    case low
+    case high
+
+    var text: String {
+        switch self {
+        case .recommend:
+            return "신작추천순"
+        case .popular:
+            return "인기순"
+        case .recent:
+            return "최신순"
+        case .low:
+            return "낮은가격순"
+        case .high:
+            return "높은가격순"
+        }
+    }
+    var descriptionText: String? {
+        switch self {
+        case .recommend:
+            return "작품 클릭 수, 작품 좋아요 수, 작가\n판매 실적 등을 기준으로 정합니다. "
+        case .popular:
+            return "작품 인기도, 작가의 팔로워, 찜하기\n등을 기준으로 정합니다."
+        default:
+            return nil
+        }
+    }
+}
