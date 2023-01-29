625187715684b1d21e9ba1aebec4d279999e7f96
Doy Kim <kimdee.dev@gmail.com>
2023-01-29 16:41:00 +0900

    [Design] #23 카테고리 태그 콜렉션뷰 셀 정의 - 열거형으로 기본과 선택 태그 케이스 만들고 색상 변수를 computed property로 추가

---
+++ Sinzak/Scene/Market/View/CategoryTagCVC.swift
@@ -0,0 +1,84 @@
+//
+//  CategoryTagCVC.swift
+//  Sinzak
+//
+//  Created by Doy Kim on 2023/01/27.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+enum ColorKind {
+    case base
+    case selected
+    var color: UIColor {
+        switch self {
+        case .base:
+            return CustomColor.gray60!
+        case .selected:
+            return CustomColor.red!
+        }
+    }
+}
+
+final class CategoryTagCVC: UICollectionViewCell {
+    let checkIcon = UIImageView().then {
+        $0.image = UIImage(named: "checkmark")?.withRenderingMode(.alwaysTemplate)
+        $0.tintColor = CustomColor.gray60
+    }
+    let categoryLabel = UILabel().then {
+        $0.font = .caption_B
+        $0.textColor = CustomColor.gray60
+        $0.textAlignment = .right
+        $0.numberOfLines = 0
+        $0.text = "전체"
+    }
+    let tagBackgroundView = UIView().then {
+        $0.clipsToBounds = true
+        $0.layer.cornerRadius = 15
+        $0.backgroundColor = CustomColor.white
+        $0.layer.borderWidth = 1
+        $0.layer.borderColor = CustomColor.gray60?.cgColor
+    }
+    // MARK: - Init
+    func setColor(kind: ColorKind) {
+        let color = kind.color
+        checkIcon.tintColor = color
+        categoryLabel.textColor = color
+        tagBackgroundView.layer.borderColor = color.cgColor
+    }
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+        setConstraints()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    // MARK: - UI
+    func setupUI() {
+        contentView.addSubviews(
+            tagBackgroundView, checkIcon, categoryLabel
+        )
+    }
+    func setConstraints() {
+        categoryLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.trailing.lessThanOrEqualToSuperview().inset(15)
+            make.top.bottom.equalToSuperview().inset(9)
+        }
+        checkIcon.snp.makeConstraints { make in
+            make.centerY.equalTo(categoryLabel)
+            make.width.height.equalTo(20)
+            make.leading.equalToSuperview().inset(8)
+            make.trailing.equalTo(categoryLabel.snp.leading).offset(-2)
+        }
+        tagBackgroundView.snp.makeConstraints { make in
+            make.leading.equalTo(checkIcon).offset(-8)
+            make.top.equalTo(categoryLabel).offset(-9)
+            make.bottom.equalTo(categoryLabel).offset(9)
+            make.trailing.equalTo(categoryLabel).offset(15)
+        }
+    }
+}
