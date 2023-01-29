edd419aaa425d1699948316c0152bbfbbe80bc7d
Doy Kim <kimdee.dev@gmail.com>
2023-01-29 16:41:32 +0900

    [Design] #23 콜렉션 뷰와 글 작성 버튼을 커스텀 뷰에 추가

--- Sinzak/Scene/Market/View/MarketView.swift
+++ Sinzak/Scene/Market/View/MarketView.swift
@@ -10,5 +10,31 @@ import SnapKit
 import Then
 
 final class MarketView: SZView {
-    
+    // MARK: - Properties
+    lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout()).then {
+        $0.backgroundColor = .clear
+    }
+    let writeButton = UIButton().then {
+        $0.clipsToBounds = true
+        $0.layer.cornerRadius = 31
+        $0.backgroundColor = CustomColor.red
+        $0.setImage(UIImage(named: "plus"), for: .normal)
+    }
+    // MARK: - Design Helpers
+    override func setView() {
+        addSubviews(
+            collectionView, writeButton
+        )
+    }
+    override func setLayout() {
+        collectionView.snp.makeConstraints { make in
+            make.trailing.leading.bottom.equalToSuperview()
+            make.top.equalTo(safeAreaLayoutGuide)
+        }
+        writeButton.snp.makeConstraints { make in
+            make.width.height.equalTo(62)
+            make.bottom.equalTo(safeAreaLayoutGuide).inset(7)
+            make.trailing.equalToSuperview().inset(11)
+        }
+    }
 }
