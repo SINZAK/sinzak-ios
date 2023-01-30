7d73b616569c62587163c9e53ba62e759c1c0785
Doy Kim <kimdee.dev@gmail.com>
2023-01-30 12:47:19 +0900

    [Feature] #35 Home 뷰모델 선언

---
+++ Sinzak/Scene/Home/ViewModel/HomeViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  HomeViewModel.swift
+//  Sinzak
+//
+//  Created by Doy Kim on 2023/01/30.
+//
+
+import Foundation
+
+class HomeViewModel: ViewModelType {
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    func transform(input: Input) -> Output {
+        return Output()
+    }
+    
+}
