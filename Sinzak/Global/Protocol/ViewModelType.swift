e7119065a1f87179e6de06bc258421629d518c5c
Doy Kim <kimdee.dev@gmail.com>
2023-01-31 01:19:59 +0900

    [Feature] #32 뷰모델타입 프로토콜에 DisposeBag 추가

--- Sinzak/Global/Protocol/ViewModelType.swift
+++ Sinzak/Global/Protocol/ViewModelType.swift
@@ -5,11 +5,12 @@
 //  Created by Doy Kim on 2023/01/26.
 //
 
+import RxSwift
 import Foundation
 
 protocol ViewModelType: AnyObject {
     associatedtype Input
     associatedtype Output
-    
+    var disposeBag: DisposeBag { get set }
     func transform(input: Input) -> Output
 }
