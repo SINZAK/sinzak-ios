5971eac1e8421dc82b03346e488864084e524b4b
Doy Kim <kimdee.dev@gmail.com>
2023-03-02 14:09:50 +0900

    [Design] #3 마이프로필 그룹핑, 프로필편집 그룹 및 View, VC 구성

---
+++ Sinzak/Scene/MyProfile/EditProfile/EditProfileVC.swift
@@ -0,0 +1,8 @@
+//
+//  EditProfileVC.swift
+//  Sinzak
+//
+//  Created by Doy Kim on 2023/03/02.
+//
+
+import Foundation
---
+++ Sinzak/Scene/MyProfile/EditProfile/EditProfileView.swift
@@ -0,0 +1,8 @@
+//
+//  EditProfileView.swift
+//  Sinzak
+//
+//  Created by Doy Kim on 2023/03/02.
+//
+
+import Foundation
