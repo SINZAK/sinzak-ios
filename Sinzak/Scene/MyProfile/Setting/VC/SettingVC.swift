4d48f0801d90e2a3e5c13597b44d08545f7af822
Doy Kim <kimdee.dev@gmail.com>
2023-03-02 16:36:57 +0900

    [Design] #33 설정화면 View, VC 구성

---
+++ Sinzak/Scene/MyProfile/Setting/VC/SettingVC.swift
@@ -0,0 +1,29 @@
+//
+//  SettingVC.swift
+//  Sinzak
+//
+//  Created by Doy Kim on 2023/03/02.
+//
+
+import UIKit
+
+final class SettingVC: SZVC {
+    // MARK: - Properties
+    private let mainView = SettingView()
+    // MARK: - Lifecycle
+    override func loadView() {
+        view = mainView
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tabBarController?.tabBar.isHidden = true
+    }
+    // MARK: - Helpers
+    override func configure() {
+        
+    }
+    override func setNavigationBar() {
+        super.setNavigationBar()
+        
+    }
+}
---
+++ Sinzak/Scene/MyProfile/Setting/View/SettingView.swift
@@ -0,0 +1,22 @@
+//
+//  SettingView.swift
+//  Sinzak
+//
+//  Created by Doy Kim on 2023/03/02.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+final class SettingView: SZView {
+    // MARK: - Properties
+    
+    // MARK: - Design Helpers
+    override func setView() {
+        
+    }
+    override func setLayout() {
+        
+    }
+}
