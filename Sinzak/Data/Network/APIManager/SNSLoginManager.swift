4fa9538bd03d79794967b8df4000b720992f574a
Doy Kim <kimdee.dev@gmail.com>
2023-03-05 13:39:31 +0900

    [Feature] #68 SNS 로그인 매니저 클래스 싱글톤패턴으로 구현, 애플로그인 메서드 추가

---
+++ Sinzak/Data/Network/APIManager/SNSLoginManager.swift
@@ -0,0 +1,33 @@
+//
+//  SNSLoginManager.swift
+//  Sinzak
+//
+//  Created by Doy Kim on 2023/03/05.
+//
+
+import Foundation
+import Moya
+
+class SNSLoginManager {
+    private init () {}
+    static let shared = SNSLoginManager()
+    let provider = MoyaProvider<SNSLoginAPI>()
+    /// 애플로그인
+    func doAppleLogin(idToken: String, origin: String = "apple",  completion: @escaping ((Result<SNSLoginResult, Error>) -> Void) ) {
+        provider.request(.apple(idToken: idToken, origin: origin)) { (result) in
+            // escaping completion handler로 처리하기
+            switch result {
+            case let .success(data):
+                do {
+                    let decoder = JSONDecoder()
+                    let result = try decoder.decode(SNSLoginResult.self, from: data.data)
+                    completion(.success(result))
+                } catch {
+                    completion(.failure(error))
+                }
+            case let .failure(error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
