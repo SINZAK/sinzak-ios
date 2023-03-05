6c63bd98e30676ea6f0b8d1d15d2f6e142166269
Doy Kim <kimdee.dev@gmail.com>
2023-03-05 17:10:07 +0900

    [Design] #69 대학생이 아닐경우 웰컴화면으로 연결,  다음버튼 누를 경우 대학생 인증 화면으로 연결하는 메서드 정의 및 액션 추가

--- Sinzak/Scene/SchoolAuth/UniversityInfo/VC/UniversityInfoVC.swift
+++ Sinzak/Scene/SchoolAuth/UniversityInfo/VC/UniversityInfoVC.swift
@@ -5,4 +5,34 @@
 //  Created by Doy Kim on 2022/12/29.
 //
 
-import Foundation
+import UIKit
+
+final class UniversityInfoVC: SZVC {
+    // MARK: - Properties
+    private let mainView = UniversityInfoView()
+    // MARK: - Lifecycle
+    override func loadView() {
+        view = mainView
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    // MARK: - Actions
+    @objc func notStudentButtonTapped(_ sender: UIButton) {
+        let vc = WelcomeVC()
+        vc.modalPresentationStyle = .fullScreen
+        present(vc, animated: true)
+    }
+    @objc func nextButtonTapped(_ sender: UIButton) {
+        let vc = SZVC()  // studentauthvc로 이동
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    // MARK: - Helpers
+    override func configure() {
+        mainView.notStudentButton.addTarget(self, action: #selector(notStudentButtonTapped), for: .touchUpInside)
+        mainView.nextButton.addTarget(self, action: #selector(nextButtonTapped), for: .touchUpInside)
+    }
+    override func setNavigationBar() {
+        super.setNavigationBar()
+    }
+}
