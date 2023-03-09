6f4d4d8b92228fc3a79e976203dee2d22caef4df
Doy Kim <kimdee.dev@gmail.com>
2023-03-09 18:36:38 +0900

    [Feature] #81 키 체인 서비스를 앱 번들 아이디로 자동 초기화 / 기존 유저 정보 -> 액세스토큰과 리프레시 토큰을 읽고 삭제할 수 있게 변경 - 토큰 저장할 때 사용하는 리터럴을 열거형으로 변경

--- Sinzak/Global/Support/KeychainItem.swift
+++ Sinzak/Global/Support/KeychainItem.swift
@@ -7,6 +7,20 @@
 
 import Foundation
 
+/** 토큰 종류 */
+enum TokenKind {
+    case accessToken
+    case refreshToken
+    var text: String {
+        switch self {
+        case .accessToken:
+            return "accessToken"
+        case .refreshToken:
+            return "refreshToken"
+        }
+    }
+}
+
 /// 제네릭 패스워드 키체인 아이템에 접근하기 위한 구조체
 struct KeychainItem {
     // MARK: - Types
@@ -20,12 +34,10 @@ struct KeychainItem {
     let service: String
     private(set) var account: String
     let accessGroup: String?
-    
     // MARK: - Init
-    init(service: String,
-         account: String,
+    init(account: String,
          accessGroup: String? = nil) {
-        self.service = service
+        self.service = "com.kimdee.Sinzak" // 현재 앱 번들 아이디
         self.account = account
         self.accessGroup = accessGroup
     }
@@ -37,17 +49,14 @@ struct KeychainItem {
         query[kSecMatchLimit as String] = kSecMatchLimitOne
         query[kSecReturnAttributes as String] = kCFBooleanTrue
         query[kSecReturnData as String] = kCFBooleanTrue
-        
         // 기존 키체인 아이템 중에 쿼리와 일치하는 아이템을 가져오기
         var queryResult: AnyObject?
         let status = withUnsafeMutablePointer(to: &queryResult) {
             SecItemCopyMatching(query as CFDictionary, UnsafeMutablePointer($0))
         }
-        
         // 리턴 상태 체크 후, 필요한 경우 에러 발생
         guard status != errSecItemNotFound else { throw KeychainError.noPassword }
         guard status == noErr else { throw KeychainError.unhandledError }
-        
         // 쿼리 결과에서 비밀번호 문자열 파싱
         guard let existingItem = queryResult as? [String: AnyObject],
             let passwordData = existingItem[kSecValueData as String] as? Data,
@@ -62,18 +71,14 @@ struct KeychainItem {
     func saveItem(_ password: String) throws {
         // 비밀번호를 Data Object로 인코딩
         let encodedPassword = password.data(using: String.Encoding.utf8)!
-        
         do {
             // 키체인에 이미 있는 항목인지 체크
             try _ = readItem()
-            
             // 이미 있을 경우 기존아이템을 새 비밀번호로 업데이트
             var attributesToUpdate = [String: AnyObject]()
             attributesToUpdate[kSecValueData as String] = encodedPassword as AnyObject?
-            
             let query = KeychainItem.keychainQuery(withService: service, account: account, accessGroup: accessGroup)
             let status = SecItemUpdate(query as CFDictionary, attributesToUpdate as CFDictionary)
-            
             // 예상에 없는 상태가 반환될 경우 에러 발생
             guard status == noErr else { throw KeychainError.unhandledError }
         } catch KeychainError.noPassword {
@@ -82,10 +87,8 @@ struct KeychainItem {
              */
             var newItem = KeychainItem.keychainQuery(withService: service, account: account, accessGroup: accessGroup)
             newItem[kSecValueData as String] = encodedPassword as AnyObject?
-            
             // 키체인에 새 아이템 추가
             let status = SecItemAdd(newItem as CFDictionary, nil)
-            
             // 에러 핸들링
             guard status == noErr else { throw KeychainError.unhandledError }
         }
@@ -95,11 +98,9 @@ struct KeychainItem {
         // 기존 아이템을 키체인에서 제거
         let query = KeychainItem.keychainQuery(withService: service, account: account, accessGroup: accessGroup)
         let status = SecItemDelete(query as CFDictionary)
-        
         // 에러핸들링
         guard status == noErr || status == errSecItemNotFound else { throw KeychainError.unhandledError }
     }
-    
     // MARK: - Convenience
     private static func keychainQuery(withService service: String, account: String? = nil, accessGroup: String? = nil) -> [String: AnyObject] {
         var query = [String: AnyObject]()
@@ -115,21 +116,36 @@ struct KeychainItem {
     }
     
     /**
-     디바이스 키체인에 userIdentifier 저장. 추후 서버단으로 변경
+     디바이스 키체인에 accessToken, refreshToken 저장
      */
-    static var currentUserIdentifier: String {
+    static var currentAccessToken: String {
         do {
-            let storedIdentifier = try KeychainItem(service: "com.kimdee.Sinzak", account: "userIdentifier").readItem()
-            return storedIdentifier
+            let storedAccessToken = try KeychainItem(account: TokenKind.accessToken.text).readItem()
+            return storedAccessToken
         } catch {
             return ""
         }
     }
-    static func deleteUserIdentifierFromKeychain() {
+    static var currentRefreshToken: String {
         do {
-            try KeychainItem(service: "com.kimdee.Sinzak", account: "userIdentifier").deleteItem()
+            let storedRefreshToken = try KeychainItem(account: TokenKind.refreshToken.text).readItem()
+            return storedRefreshToken
         } catch {
-            print("키체인에서 userIdentifier를 제거할 수 없습니다.")
+            return ""
+        }
+    }
+    static func deleteAccessTokenFromKeychain() {
+        do {
+            try KeychainItem(account: TokenKind.accessToken.text).deleteItem()
+        } catch {
+            print("키체인에서 accessToken를 제거할 수 없습니다.")
+        }
+    }
+    static func deleteRefreshTokenFromKeychain() {
+        do {
+            try KeychainItem(account: TokenKind.refreshToken.text).deleteItem()
+        } catch {
+            print("키체인에서 refreshToken를 제거할 수 없습니다.")
         }
     }
 }
