feaa33e72a98acf9e3083cb1217fd7778f3303bf
Doy Kim <kimdee.dev@gmail.com>
2023-03-10 16:40:43 +0900

    [Feature] #77 바뀐 회원가입 타겟타입과 추가된 API 적용한 메서드 추가 - 닉네임 중복확인, 회원가입, 회원정보 추가,  관심장르 편집, FCM 토큰 업데이트

--- Sinzak/Data/Network/APIManager/AuthManager.swift
+++ Sinzak/Data/Network/APIManager/AuthManager.swift
@@ -5,42 +5,103 @@
 //  Created by Doy Kim on 2023/01/08.
 //
 
+import Foundation
 import Moya
 
 class AuthManager {
+    // MARK: - Properties
     private init () {}
     static let shared = AuthManager()
     let provider = MoyaProvider<AuthAPI>()
-    /// 이메일 중복 확인
-    func checkEmail(_ email: String) {
-        provider.request(.checkemail(email: email)) { (result) in
-            // escaping completion handler로 처리하기 
+    // MARK: - Methods
+    /// 닉네임 중복 확인
+    func checkNickname(for nickname: String, completion: @escaping ((Bool) -> Void)) {
+        provider.request(.nicknameCheck(nickname: nickname)) { result in
             switch result {
-            case let .success(response):
-                if response.response?.statusCode == 200 {
-                    print("가입할 수 있는 이메일")
-                } else {
-                    print("가입할 수 없는 이메일")
+            case let .success(data):
+                do { let decoder = JSONDecoder()
+                    let result = try decoder.decode(ShortMessageResult.self, from: data.data)
+                    completion(result.success)
+                } catch {
+                    completion(false)
                 }
             case let .failure(error):
-                print(error.localizedDescription)
+                print("Nickname check error", error)
+                completion(false)
             }
         }
     }
     /// 회원가입
-    func join(_ userInfo: JoinUser) {
-        provider.request(.signup(userInfo: userInfo)) { (result) in
+    func join(_ joinInfo: Join, completion: @escaping ((Bool) -> Void)) {
+        provider.request(.join(joinInfo: joinInfo)) { result in
             switch result {
-            case let .success(response):
-                if response.response?.statusCode == 200 {
-                    print("가입완료")
-                } else {
-                    print("가입불가")
+            case .success(let response):
+                do {
+                    let result = try response.map(OnlySuccess.self)
+                    completion(result.success)
+                } catch {
+                    print("Error decoding JSON: \(error.localizedDescription)")
+                    completion(false)
                 }
-            case let .failure(error):
+            case .failure(let error):
                 print(error.localizedDescription)
+                completion(false)
+            }
+        }
+    }
+    /// 회원정보 추가, 편집
+    /// - 자기소개, 이름, 사진
+    func editUserInfo(_ userInfo: UserInfo, completion: @escaping ((Bool)-> Void)) {
+        provider.request(.editUserInfo(userInfo: userInfo)) { result in
+            switch result {
+            case .success(let response):
+                do {
+                    let result = try response.map(OnlySuccess.self)
+                    completion(result.success)
+                } catch {
+                    print("Error decoding JSON: \(error.localizedDescription)")
+                    completion(false)
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+                completion(false)
+            }
+        }
+    }
+    /// 관심장르 편집
+    func editCategory(_ category: CategoryLikeEdit, completion: @escaping ((Bool)-> Void)) {
+        provider.request(.editCategoryLike(category: category)) { result in
+            switch result {
+            case .success(let response):
+                do {
+                    let result = try response.map(OnlySuccess.self)
+                    completion(result.success)
+                } catch {
+                    print("Error decoding JSON: \(error.localizedDescription)")
+                    completion(false)
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+                completion(false)
+            }
+        }
+    }
+    /// FCM 토큰 업데이트
+    func updateFCMToken(_ token: FCMTokenUpdate, completion: @escaping ((Bool)-> Void)) {
+        provider.request(.fcmTokenUpdate(fcmInfo: token)) { result in
+            switch result {
+            case .success(let response):
+                do {
+                    let result = try response.map(OnlySuccess.self)
+                    completion(result.success)
+                } catch {
+                    print("Error decoding JSON: \(error.localizedDescription)")
+                    completion(false)
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+                completion(false)
             }
         }
     }
 }
-
