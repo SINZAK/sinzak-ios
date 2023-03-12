7b7b05be36cf368df3ac739516de01f1a72d4e64
Doy Kim <kimdee.dev@gmail.com>
2023-03-12 14:02:00 +0900

    [Feature] #77 사용자 관련 추가된 API 업데이트 - 사용자 프로필 이미지 - FCM 토큰 업데이트 - 검색기록 삭제 - 팔로우, 언팔로우 - 신고, 신고취소, 신고목록 - 탈퇴

--- Sinzak/Data/Network/API/AuthAPI.swift
+++ Sinzak/Data/Network/API/AuthAPI.swift
@@ -5,16 +5,32 @@
 //  Created by Doy Kim on 2023/01/08.
 //
 
-import Foundation
+import UIKit
 import Moya
 
 enum AuthAPI {
+    // 가입 및 사용자 프로필 수정
     case nicknameCheck(nickname: String)
     case join(joinInfo: Join)
+    case editUserImage(image: UIImage)
     case editUserInfo(userInfo: UserInfo)
     case editCategoryLike(category: CategoryLikeEdit)
-    case fcmTokenUpdate(fcmInfo: FCMTokenUpdate)
-    case reissue // 토큰 갱신
+    // 액세스토큰, FCM 토큰
+    case fcmTokenUpdate(fcmInfo: FCMTokenUpdate) // fcm 토큰 업데이트 ㅡ
+    case reissue // 액세스 토큰 갱신
+    // 검색기록 삭제
+    case deleteSearchHistory // 검색기록 전체 삭제
+    case deleteOneHistory(id: Int) // 검색기록 한 개 삭제
+    // 팔로우
+    case follow(userId: String)
+    case unfollow(userId: String)
+    // 신고
+    case report(reason: String, userId: String) // 신고
+    case cancelReport(userId: String) // 신고 취소
+    case reportList // 신고목록 보기
+    // 탈퇴
+    case resign
+    
 }
 
 extension AuthAPI: TargetType {
@@ -27,6 +43,8 @@ extension AuthAPI: TargetType {
             return "/check/nickname"
         case .join:
             return "/join"
+        case .editUserImage:
+            return "/users/edit/image"
         case .editUserInfo:
             return "/users/edit"
         case .editCategoryLike:
@@ -35,11 +53,27 @@ extension AuthAPI: TargetType {
             return "/users/fcm"
         case .reissue:
             return "/reissue"
+        case .deleteSearchHistory:
+            return "/users/deletehistories"
+        case .deleteOneHistory:
+            return "/users/history"
+        case .follow:
+            return "/users/follow"
+        case .unfollow:
+            return "/users/unfollow"
+        case .report:
+            return "/users/report"
+        case .cancelReport:
+            return "/users/report/cancel"
+        case .reportList:
+            return "/users/reportlist"
+        case .resign:
+            return "/users/resign"
         }
     }
     var method: Moya.Method {
         switch self {
-        case .nicknameCheck, .join, .editUserInfo, .fcmTokenUpdate, .reissue, .editCategoryLike:
+        default:
             return .post
         }
     }
@@ -60,6 +94,12 @@ extension AuthAPI: TargetType {
                     print("Error encoding userInfo: \(error)")
                     return .requestPlain
                 }
+        case .editUserImage(let image):
+            var formData: [MultipartFormData] = []
+            guard let imageData = image.jpegData(compressionQuality: 0.6) else { return .uploadMultipart(formData)}
+            let imageName = String.uniqueFilename(withPrefix: "IMAGE")
+            formData.append(MultipartFormData(provider: .data(imageData), name: imageName, fileName: "\(imageName).jpg", mimeType: "image/jpg"))
+            return .uploadMultipart(formData)
         case .editUserInfo(let userInfo):
             do {
                 let encoder = JSONEncoder()
@@ -91,6 +131,32 @@ extension AuthAPI: TargetType {
                 "refreshToken": KeychainItem.currentRefreshToken
             ]
             return .requestParameters(parameters: params, encoding: URLEncoding.queryString)
+            
+        case .deleteOneHistory(let id):
+            let params: [String: Any] = [
+                "id": id
+            ]
+            return .requestParameters(parameters: params, encoding: URLEncoding.queryString)
+            
+        case .follow(let userId), .unfollow(let userId):
+            let params: [String: Any] = [
+                "userId": userId
+            ]
+            return .requestParameters(parameters: params, encoding: URLEncoding.queryString)
+        case .report(let reason, let userId):
+            let params: [String: Any] = [
+                "reason": reason,
+                "userId": userId
+            ]
+            return .requestParameters(parameters: params, encoding: URLEncoding.queryString)
+            
+        case .cancelReport(let userId):
+            let params: [String: Any] = [
+                "userId": userId
+            ]
+            return .requestParameters(parameters: params, encoding: URLEncoding.queryString)
+        case .deleteSearchHistory, .reportList, .resign:
+            return .requestPlain
         }
     }
     var headers: [String: String]? {
@@ -99,7 +165,17 @@ extension AuthAPI: TargetType {
         ]
         let accessToken = KeychainItem.currentAccessToken
         switch self {
-        case .join, .editUserInfo, .fcmTokenUpdate, .editCategoryLike:
+        case .nicknameCheck, .reissue:
+            return header
+            
+        case .editUserImage:
+            var header = [
+                "Content-Type": "multipart/form-data; boundary=<calculated when request is sent>"
+            ]
+            header["Authorization"] = accessToken
+            return header
+            
+        default:
             if !accessToken.isEmpty {
                 var header = header
                 header["Authorization"] = accessToken
@@ -108,8 +184,6 @@ extension AuthAPI: TargetType {
                 print("액세스토큰이 없어 불가능합니다.")
                 return header
             }
-        case .nicknameCheck, .reissue:
-            return header
         }
     }
 }
