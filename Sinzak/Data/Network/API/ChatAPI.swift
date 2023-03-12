30baa81f2c6428ee5f0e2401499ebfea0937189a
Doy Kim <kimdee.dev@gmail.com>
2023-03-12 14:52:12 +0900

    [Feature] #87 채팅 API 명령쪽 타겟타입 추가 - 이미지 업로드 - 채팅방 생성

--- Sinzak/Data/Network/API/ChatAPI.swift
+++ Sinzak/Data/Network/API/ChatAPI.swift
@@ -5,14 +5,18 @@
 //  Created by Doy Kim on 2023/03/04.
 //
 
-import Foundation
+import UIKit
 import Moya
 
 enum ChatAPI {
+    // 채팅방 조회
     case chatRoomList // 챗룸 리스트
     case chatRoom(uuid: String) // 특정 챗룸 정보 조회
     case chatRoomMessages(uuid: String) // 메시지 조회
     case allChatRoomForPost(postId: Int, postType: String)
+    // 채팅방 생성, 이미지 업로드
+    case chatCreate(postId: Int, postType: String)
+    case chatRoomImageUpload(uuid: String, images: [UIImage])
 }
 
 extension ChatAPI: TargetType {
@@ -29,6 +33,10 @@ extension ChatAPI: TargetType {
             return "/chat/rooms/\(uuid)/message"
         case .allChatRoomForPost:
             return "/chat/rooms/post"
+        case .chatCreate:
+            return "/chat/rooms/create"
+        case .chatRoomImageUpload(let uuid, _):
+            return "/chat/rooms/\(uuid)/image"
         }
     }
     var method: Moya.Method {
@@ -43,12 +51,25 @@ extension ChatAPI: TargetType {
         switch self {
         case .chatRoomList, .chatRoom, .chatRoomMessages:
             return .requestPlain
-        case .allChatRoomForPost(let postId, let postType):
+        case .allChatRoomForPost(let postId, let postType), .chatCreate(let postId, let postType):
             let params: [String: Any] = [
                 "postId": postId,
                 "postType": postType
             ]
             return .requestParameters(parameters: params, encoding: URLEncoding.queryString)
+        case .chatRoomImageUpload(_, let images):
+            var formData: [MultipartFormData] = []
+            for image in images {
+                if let imageData = image.jpegData(compressionQuality: 0.6) {
+                    let name = String.uniqueFilename(withPrefix: "IMAGE")
+                    formData.append(MultipartFormData(
+                        provider: .data(imageData),
+                        name: name,
+                        fileName: "\(name).jpg"
+                    ))
+                }
+            }
+            return .uploadMultipart(formData)
         }
     }
     var headers: [String : String]? {
@@ -57,6 +78,10 @@ extension ChatAPI: TargetType {
         ]
         let accessToken = KeychainItem.currentAccessToken
         switch self {
+        case .chatRoomImageUpload:
+            var header = ["Content-Type": "multipart/form-data; boundary=<calculated when request is sent>" ]
+            header["Authorization"] = accessToken
+            return header 
         default:
             if !accessToken.isEmpty {
                 var header = header
