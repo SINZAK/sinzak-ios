264454450d133648589e20384ecf74ed49563c60
Doy Kim <kimdee.dev@gmail.com>
2023-03-12 22:17:11 +0900

    [Feature] #77 키보드 높이에 따라 다음 버튼 위치 변경하도록 rxkeyboard 로 이용

--- Sinzak/Scene/Signup/VC/SignupNameVC.swift
+++ Sinzak/Scene/Signup/VC/SignupNameVC.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import RxKeyboard
 
 final class SignupNameVC: SZVC {
     // MARK: - Properties
@@ -28,5 +31,28 @@ final class SignupNameVC: SZVC {
     // MARK: - Helpers
     override func configure() {
         mainView.nextButton.addTarget(self, action: #selector(nextButtonTapped), for: .touchUpInside)
+        bind()
+    }
+    func bind() {
+        RxKeyboard.instance.visibleHeight
+         .drive(onNext: { [unowned self] keyboardHeight in
+             print("keyBoard 높이는 \(keyboardHeight) 입니다.")
+             if keyboardHeight > 0 {
+                 self.mainView.nextButton.snp.updateConstraints({ make in
+                     make.bottom.equalTo(self.mainView.safeAreaLayoutGuide).offset(-keyboardHeight + self.mainView.safeAreaInsets.bottom)
+                     make.leading.trailing.equalTo(self.mainView)
+                     self.mainView.nextButton.layer.cornerRadius = 0
+                     self.mainView.layoutIfNeeded()
+                 })
+             } else {
+                 self.mainView.nextButton.snp.updateConstraints({ make in
+                     make.bottom.equalTo(self.mainView.safeAreaLayoutGuide)
+                     make.leading.trailing.equalTo(self.mainView).inset(7.4)
+                     self.mainView.nextButton.layer.cornerRadius = 30
+                     self.mainView.layoutIfNeeded()
+                 })
+             }
+         })
+         .disposed(by: viewModel.disposeBag)
     }
 }
