3ff3562b6818fb1d3155e77b5161aca67c7534bb
Doy Kim <kimdee.dev@gmail.com>
2023-03-12 15:01:04 +0900

    [Feature] #83, #84 마켓 글, 의뢰 글 작성용 모델 구조체 정의

---
+++ Sinzak/Scene/Write/Model/MarketBuild.swift
@@ -0,0 +1,28 @@
+//
+//  MarketBuild.swift
+//  Sinzak
+//
+//  Created by Doy Kim on 2023/02/27.
+//
+
+import Foundation
+
+struct MarketBuild: Codable {
+    let category: String
+    let title: String
+    let content: String
+    let height, vertical, width: Int
+    let suggest: Bool
+    let price: Int
+    
+    init(category: Category, title: String, content: String, height: Int, vertical: Int, width: Int, suggest: Bool, price: Int) {
+        self.category = category.rawValue
+        self.title = title
+        self.content = content
+        self.height = height
+        self.vertical = vertical
+        self.width = width
+        self.suggest = suggest
+        self.price = price
+    }
+}
---
+++ Sinzak/Scene/Write/Model/WorkBuild.swift
@@ -0,0 +1,26 @@
+//
+//  WorkBuild.swift
+//  Sinzak
+//
+//  Created by Doy Kim on 2023/03/12.
+//
+
+import Foundation
+
+struct WorkBuild: Codable {
+    let category: String
+    let title: String
+    let content: String
+    let employment: Bool
+    let price: Int
+    let suggest: Bool
+    
+    init(category: Category, title: String, content: String, employment: Bool, price: Int, suggest: Bool) {
+        self.category = category.rawValue
+        self.title = title
+        self.content = content
+        self.employment = employment
+        self.price = price
+        self.suggest = suggest
+    }
+}
