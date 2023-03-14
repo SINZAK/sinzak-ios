5928227cf7f4399b93012371c5c26493c8ccf569
Doy Kim <kimdee.dev@gmail.com>
2023-03-14 09:53:38 +0900

    [Feature] #77 회원가입 뷰모델에 Input-Output 패턴 적용. 이름 유효성 검사 관련하여 설정

--- Sinzak/Scene/Signup/ViewModel/SignupViewModel.swift
+++ Sinzak/Scene/Signup/ViewModel/SignupViewModel.swift
@@ -7,13 +7,14 @@
 
 import Foundation
 import RxSwift
+import RxCocoa
 import Moya
 
 final class SignupViewModel: ViewModelType {
     var disposeBag = DisposeBag()
     var joinInfo = Join(categoryLike: [], nickname: "", term: false)
     let manager = AuthManager.shared
-    
+    /// 닉네임 체크 메서드
     func checkNickname(for nickname: String) -> Bool {
         var bool = false
         manager.checkNickname(for: nickname) { result in
@@ -22,13 +23,17 @@ final class SignupViewModel: ViewModelType {
         return bool
     }
     struct Input {
-        
+        let nameText: ControlProperty<String?>
     }
     struct Output {
-        
+        let nameValidation: Observable<Bool>
     }
     func transform(input: Input) -> Output {
-        return Output()
+        let nameValidation = input.nameText
+            .orEmpty
+            .map { $0.isValidString(.nickname)}
+            .share()
+        return Output(nameValidation: nameValidation)
     }
 
 }
