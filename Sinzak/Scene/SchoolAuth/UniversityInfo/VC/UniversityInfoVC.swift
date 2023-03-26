f56534f16ac9ff65107b97de57223613fd31fed1
Doy Kim <kimdee.dev@gmail.com>
2023-03-26 19:32:30 +0900

    [Feature] 학교검색 화면에서 기존 버튼액션을 rx로 변경, 텍스트필드 값을 받아서 학교 데이터 검색하여 자동완성 구현

--- Sinzak/Scene/SchoolAuth/UniversityInfo/VC/UniversityInfoVC.swift
+++ Sinzak/Scene/SchoolAuth/UniversityInfo/VC/UniversityInfoVC.swift
@@ -6,33 +6,63 @@
 //
 
 import UIKit
+import RxSwift
 
 final class UniversityInfoVC: SZVC {
     // MARK: - Properties
     private let mainView = UniversityInfoView()
+    let viewModel = SchoolAuthViewModel()
+    var schoolList: [String] = []
+    var filteredData: [String] = []
+    
     // MARK: - Lifecycle
     override func loadView() {
         view = mainView
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        schoolList = SchoolList.loadJson()!.school.flatMap { $0.koreanName }
     }
     // MARK: - Actions
-    @objc func notStudentButtonTapped(_ sender: UIButton) {
-        let vc = WelcomeVC()
-        vc.modalPresentationStyle = .fullScreen
-        present(vc, animated: true)
-    }
-    @objc func nextButtonTapped(_ sender: UIButton) {
-        let vc = StudentAuthVC()
-        navigationController?.pushViewController(vc, animated: true)
-    }
     // MARK: - Helpers
     override func configure() {
-        mainView.notStudentButton.addTarget(self, action: #selector(notStudentButtonTapped), for: .touchUpInside)
-        mainView.nextButton.addTarget(self, action: #selector(nextButtonTapped), for: .touchUpInside)
+        bind()
     }
     override func setNavigationBar() {
         super.setNavigationBar()
+        navigationItem.leftBarButtonItem = nil // 이미 가입이 끝난 상황이라 뒤로 돌아가면 안됨
+    }
+    func bind() {
+        let input = SchoolAuthViewModel.Input(queryText: mainView.searchTextField.rx.text, nextButtonTap: mainView.nextButton.rx.tap, notStudentButtonTap: mainView.notStudentButton.rx.tap)
+        let output = viewModel.transform(input: input)
+        output.nextButtonTap
+            .bind { [weak self] _ in
+                guard let self = self else { return }
+                let vc = StudentAuthVC()
+                self.navigationController?.pushViewController(vc, animated: true)
+            }
+            .disposed(by: viewModel.disposeBag)
+        output.notStudentButtonTap
+            .bind { [weak self] _ in
+                guard let self = self else { return }
+                let vc = WelcomeVC()
+                vc.modalPresentationStyle = .fullScreen
+                self.present(vc, animated: true)
+            }
+            .disposed(by: viewModel.disposeBag)
+        output.queryText
+            .bind { [weak self] query in
+                guard let self = self else { return }
+                if query.count > 0 {
+                    self.filteredData.removeAll(keepingCapacity: false)
+                    let searchPredicate = NSPredicate(format: "SELF CONTAINS %@", query)
+                    let array = (self.schoolList as NSArray).filtered(using: searchPredicate)
+                    self.filteredData = array as! [String]
+                    print(self.filteredData)
+                    /// 이제 검색목록을 뷰에 뿌리기
+                } else {
+                    self.filteredData.removeAll(keepingCapacity: false)
+                }
+            }.disposed(by: viewModel.disposeBag)
     }
 }
