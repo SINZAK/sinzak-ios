fa2f6add65bc2739b26a28f0568502b285efa956
Doy Kim <kimdee.dev@gmail.com>
2023-03-26 22:04:42 +0900

    [Feature] 학교인증 request용 모델 정의

---
+++ Sinzak/Scene/SchoolAuth/Model/UnivCertify.swift
@@ -0,0 +1,14 @@
+//
+//  UnivCertify.swift
+//  Sinzak
+//
+//  Created by Doy Kim on 2023/03/26.
+//
+
+import Foundation
+
+struct UnivMailCertify {
+    var code: Int?
+    var univName: String
+    var univEmail: String
+}
