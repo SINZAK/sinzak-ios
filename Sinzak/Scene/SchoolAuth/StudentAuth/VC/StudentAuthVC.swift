fbba58b934430215f8233d34ba2f63e2675b3b80
Doy Kim <kimdee.dev@gmail.com>
2023-03-27 09:43:54 +0900

    [Feature] 사진 업로드 로직 추가

--- Sinzak/Scene/SchoolAuth/StudentAuth/VC/StudentAuthVC.swift
+++ Sinzak/Scene/SchoolAuth/StudentAuth/VC/StudentAuthVC.swift
@@ -6,11 +6,16 @@
 //
 
 import UIKit
+import YPImagePicker
+import RxSwift
+import RxCocoa
 
 final class StudentAuthVC: SZVC {
     // MARK: - Properties
     private let mainView = StudentAuthView()
     var authType = SchoolAuthType.webmail
+    var viewModel = SchoolAuthViewModel()
+    var schoolcardImage: UIImage? = UIImage()
     // MARK: - Lifecycle
     override func loadView() {
         view = mainView
@@ -51,27 +56,53 @@ final class StudentAuthVC: SZVC {
         vc.modalPresentationStyle = .fullScreen
         present(vc, animated: true)
     }
-    @objc func photoUploadButtonTapped(_ sender: UIButton) {
-        // 사진 첨부가 성공했을 경우
-        mainView.photoUploadButton.isHidden = true
-        mainView.uploadedPhotoView.isHidden = false
-    }
-    @objc func cancelPhotoButtonTapped(_ sender: UIButton) {
-        // 이미지 삭제할지 알람 띄우기
-        mainView.photoUploadButton.isHidden = false
-        mainView.uploadedPhotoView.isHidden = true
-    }
     // MARK: - Helpers
     override func configure() {
         mainView.webmailButton.addTarget(self, action: #selector(webmailButtonTapped), for: .touchUpInside)
         mainView.schoolcardButton.addTarget(self, action: #selector(schoolCardButtonTapped), for: .touchUpInside)
         mainView.doNextButton.addTarget(self, action: #selector(doNextButtonTapped), for: .touchUpInside)
         mainView.finishButton.addTarget(self, action: #selector(finishButtonTapped), for: .touchUpInside)
-        mainView.photoUploadButton.addTarget(self, action: #selector(photoUploadButtonTapped), for: .touchUpInside)
-        mainView.cancelButton.addTarget(self, action: #selector(cancelPhotoButtonTapped), for: .touchUpInside)
+        bind()
     }
     override func setNavigationBar() {
         super.setNavigationBar()
         navigationItem.title = I18NStrings.schoolAuth
+    }
+    func bind() {
+        mainView.photoUploadButton.rx
+            .tap
+            .withUnretained(self)
+            .bind { (vc, _) in
+                let picker = YPImagePicker()
+                picker.didFinishPicking { [unowned picker] items, _ in
+                    if let photo = items.singlePhoto {
+                        // 사진 첨부가 성공했을 경우
+                        vc.schoolcardImage = photo.image
+                        vc.mainView.photoUploadButton.isHidden = true
+                        vc.mainView.photoNameLabel.text = String.uniqueFilename(withPrefix: "IMG", fileExtension: "jpg")
+                        vc.mainView.uploadedPhotoView.isHidden = false
+                        vc.mainView.selectedPhoto.isHidden = false
+                        vc.mainView.selectedPhoto.image = photo.image
+                    }
+                    picker.dismiss(animated: true, completion: nil)
+                }
+                vc.present(picker, animated: true, completion: nil)
+            }
+            .disposed(by: viewModel.disposeBag)
+        mainView.cancelButton.rx
+            .tap
+            .withUnretained(self)
+            .bind { (vc, _) in
+                // 이미지 삭제할지 알람 띄우기
+                vc.showAlert(title: "이미지를 삭제하시겠습니까?", okText: "삭제", cancelNeeded: true) { _ in
+                    vc.mainView.photoUploadButton.isHidden = false
+                    vc.mainView.uploadedPhotoView.isHidden = true
+                    vc.mainView.selectedPhoto.isHidden = true
+                    // 사진 삭제
+                    vc.schoolcardImage = nil
+                }
+            }
+            .disposed(by: viewModel.disposeBag)
+
     }
 }
