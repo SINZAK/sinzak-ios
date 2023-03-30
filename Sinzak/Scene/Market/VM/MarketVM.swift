019d0ce03b7189113133507c42e4abdf71083413
JongHooon <qnm541@gmail.com>
2023-03-30 21:45:00 +0900

    [Refactor] MarketVM 생성

---
+++ Sinzak/Scene/Market/VM/MarketVM.swift
@@ -0,0 +1,26 @@
+//
+//  MarketVM.swift
+//  Sinzak
+//
+//  Created by JongHoon on 2023/03/30.
+//
+
+import Foundation
+import RxSwift
+
+protocol MarketVMInput {
+    
+}
+
+protocol MarketVMOutput {
+    
+}
+
+protocol MarketVM: MarketVMInput, MarketVMOutput {}
+
+final class DefaultMarketVM: MarketVM {
+    
+    
+    
+    
+}
