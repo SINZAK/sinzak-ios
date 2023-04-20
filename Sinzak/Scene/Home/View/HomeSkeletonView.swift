ae2c20ba086f57aad93072e81f9c46b7648367a9
JongHooon <qnm541@gmail.com>
2023-04-21 00:11:35 +0900

    [Refactor] Home View - layout 수정

--- Sinzak/Scene/Home/View/HomeSkeletonView.swift
+++ Sinzak/Scene/Home/View/HomeSkeletonView.swift
@@ -17,33 +17,58 @@ class HomeSkeletonView: SZView {
         $0.isSkeletonable = true
     }
     
-    let productFlowLayout: UICollectionViewLayout = {
+    let label1: UILabel = {
+        let label = UILabel()
+        label.font = .subtitle_B
+        label.text = "최신 작품작품작품"
+        label.textColor = .clear
+        label.isSkeletonable = true
+        
+        return label
+    }()
+    
+    let label2: UILabel = {
+        let label = UILabel()
+        label.font = .subtitle_B
+        label.text = "최신 작품작품작품"
+        label.textColor = .clear
+        label.isSkeletonable = true
+        
+        return label
+    }()
+    
+    lazy var flowLayout: UICollectionViewLayout = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
         layout.estimatedItemSize = CGSize(width: 165, height: 240)
         layout.sectionInset = UIEdgeInsets(top: 0, left: 40, bottom: 0, right: 0)
-//        layout.collectionViewContentSize
-//        layout.minimumLineSpacing = 16.0
-//        layout.minimumInteritemSpacing = 28.0
-        //            layout.headerReferenceSize = CGSize(width: 10, height: 40)
-        //            layout.headerReferenceSize = CGSize(width: width, height: 40)
-        //            layout.minimumLineSpacing =
+        layout.minimumLineSpacing = 40.0
         
         return layout
     }()
     
-    let collectionView: UICollectionView = {
+    lazy var collectionView1: UICollectionView = {
         let collectionView = UICollectionView(
             frame: .zero,
-            collectionViewLayout: UICollectionViewLayout()
+            collectionViewLayout: flowLayout
         )
-        
-        let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .horizontal
-        layout.estimatedItemSize = CGSize(width: 165, height: 240)
-        layout.sectionInset = UIEdgeInsets(top: 0, left: 40, bottom: 0, right: 0)
-        
-        collectionView.collectionViewLayout = layout
+                
+        collectionView.backgroundColor = .clear
+        collectionView.isSkeletonable = true
+        collectionView.register(
+            ArtCVC.self,
+            forCellWithReuseIdentifier: ArtCVC.identifier
+        )
+
+        return collectionView
+    }()
+    
+    lazy var collectionView2: UICollectionView = {
+        let collectionView = UICollectionView(
+            frame: .zero,
+            collectionViewLayout: flowLayout
+        )
+                
         collectionView.backgroundColor = .clear
         collectionView.isSkeletonable = true
         collectionView.register(
@@ -58,9 +83,13 @@ class HomeSkeletonView: SZView {
     override func setView() {
         self.isHidden = true
         self.isSkeletonable = true
+        
         addSubviews(
             imageView,
-            collectionView
+            label1,
+            collectionView1,
+            label2,
+            collectionView2
         )
     }
     override func setLayout() {
@@ -71,9 +100,26 @@ class HomeSkeletonView: SZView {
             $0.height.equalTo(imageView.snp.width).multipliedBy(0.42)
         }
         
-        collectionView.snp.makeConstraints { make in
-            make.top.equalTo(imageView.snp.bottom).offset(32.0)
-            make.trailing.leading.bottom.equalToSuperview()
+        label1.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(24.0)
+            $0.top.equalTo(imageView.snp.bottom).offset(32.0)
+        }
+
+        collectionView1.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview()
+            $0.top.equalTo(label1.snp.bottom).offset(28.0)
+            $0.height.equalTo(248)
+        }
+        
+        label2.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(24.0)
+            $0.top.equalTo(collectionView1.snp.bottom).offset(28.0)
+        }
+        
+        collectionView2.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview()
+            $0.top.equalTo(label2.snp.bottom).offset(28.0)
+            $0.height.equalTo(248)
         }
     }
 }
