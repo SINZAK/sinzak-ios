507582847996671ab818aa513d88f582de000e23
JongHooon <qnm541@gmail.com>
2023-04-22 05:31:51 +0900

    [Feature] Reissue model 생성

---
+++ Sinzak/Scene/Login/Model/Reissue.swift
@@ -0,0 +1,15 @@
+//
+//  Reissue.swift
+//  Sinzak
+//
+//  Created by JongHoon on 2023/04/22.
+//
+
+import Foundation
+
+struct Reissue: Codable {
+    let grantType, accessToken, refreshToken: String
+    let accessTokenExpireDate: Int
+    let joined: Bool
+    let origin: String
+}
