e9b62d7722011ed5d4abb2afa7848dd4d724946f
JongHooon <qnm541@gmail.com>
2023-04-24 18:12:04 +0900

    [Refactor] HomeManager 메소드 retry 추가, 코드 수정

--- Sinzak/Data/Network/APIManager/HomeManager.swift
+++ Sinzak/Data/Network/APIManager/HomeManager.swift
@@ -19,58 +19,24 @@ class HomeManager {
     
     func getHomeProductLoggedIn() -> Single<HomeLoggedInProducts> {
         return provider.rx.request(.homeLogined)
-            .observe(on: ConcurrentDispatchQueueScheduler(queue: .global()))
-            .map({ response in
-                Log.debug(response.request?.url ?? "")
-                
-                if !(200..<300 ~= response.statusCode) {
-                    throw APIError.badStatus(code: response.statusCode)
-                }
-                
-                do {
-                    let products = try JSONDecoder().decode(HomeLoggedInProductsResponse.self, from: response.data)
-                    return products.data
-                } catch {
-                    throw APIError.decodingError
-                }
-            })
+            .filterSuccessfulStatusCodes()
+            .map(HomeLoggedInProductsResponse.self)
+            .map { $0.data }
+            .retry(2)
     }
 
     func getHomeProductNotLoggedIn() -> Single<HomeNotLoggedInProducts> {
         return provider.rx.request(.homeNotLogined)
-            .observe(on: ConcurrentDispatchQueueScheduler(queue: .global()))
-            .map({ response in
-                Log.debug(response.request?.url ?? "")
-                
-                if !(200..<300 ~= response.statusCode) {
-                    throw APIError.badStatus(code: response.statusCode)
-                }
-                
-                do {
-                    let products = try JSONDecoder().decode(HomeNotLoggedInProductsResponse.self, from: response.data)
-                    return products.data
-                } catch {
-                    throw APIError.decodingError
-                }
-            })
+            .filterSuccessfulStatusCodes()
+            .map(HomeNotLoggedInProductsResponse.self)
+            .map { $0.data }
+            .retry(2)
     }
     
     func getBannerInfo() -> Single<[Banner]> {
         return provider.rx.request(.banner)
-            .observe(on: ConcurrentDispatchQueueScheduler(queue: .global()))
-            .map({ response in
-                Log.debug(response.request?.url ?? "")
-
-                if !(200..<300 ~= response.statusCode) {
-                    throw APIError.badStatus(code: response.statusCode)
-                }
-                
-                do {
-                    let banners = try JSONDecoder().decode(BannerList.self, from: response.data)
-                    return banners.data
-                } catch {
-                    throw APIError.decodingError
-                }
-            })
+            .map(BannerList.self)
+            .map { $0.data }
+            .retry(2)
     }
 }
