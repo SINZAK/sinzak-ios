e7e601b06982fc6186bcdc507eaa05f712321199
JongHooon <qnm541@gmail.com>
2023-04-26 02:49:01 +0900

    [Feature] LikeResponseDTO 추가

---
+++ Sinzak/Data/Network/DTO/LikeResponseDTO.swift
@@ -0,0 +1,13 @@
+//
+//  LikeResponseDTO.swift
+//  Sinzak
+//
+//  Created by JongHoon on 2023/04/25.
+//
+
+import Foundation
+
+struct LikeResponseDTO: Codable {
+    let isLike: Bool
+    let success: Bool
+}
