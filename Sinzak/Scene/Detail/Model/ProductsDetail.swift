4aaa4896f977b58d7198c5090384b5af6f63ceb7
JongHooon <qnm541@gmail.com>
2023-04-26 02:16:15 +0900

    [Feature] ProductsDetatil Model 생성

---
+++ Sinzak/Scene/Detail/Model/ProductsDetail.swift
@@ -0,0 +1,39 @@
+//
+//  ProductsDetail.swift
+//  Sinzak
+//
+//  Created by JongHoon on 2023/04/25.
+//
+
+import Foundation
+
+struct ProductsDetail {
+    let id: Int
+    let userID: Int
+    let author: String
+    let authorPicture: String
+    let univ: String?
+    let certUni: Bool
+    let certAuthor: Bool
+    let followerNum: String
+    let following: Bool
+    let images: [String]?
+    let title: String
+    let category: String
+    let date: String
+    let content: String?
+    let price: Int
+    let topPrice: Int
+    let suggest: Bool
+    let like: Bool
+    let likesCnt: Int
+    let wish: Bool
+    let views: Int
+    let wishCnt: Int
+    let chatCnt: Int
+    let myPost: Bool
+    let complete: Bool
+    let width: Int
+    let vertical: Int
+    let height: Int
+}
