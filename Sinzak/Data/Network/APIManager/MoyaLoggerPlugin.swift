3a1ee4557f7049d1cdb1bbbf88ad6ba06645762b
JongHooon <qnm541@gmail.com>
2023-04-26 02:51:03 +0900

    [Refactor] MoyaLoggerPlugin log 수정

--- Sinzak/Data/Network/APIManager/MoyaLoggerPlugin.swift
+++ Sinzak/Data/Network/APIManager/MoyaLoggerPlugin.swift
@@ -16,11 +16,12 @@ final class MoyaLoggerPlugin: PluginType {
     let disposeBag = DisposeBag()
     
     func willSend(_ request: RequestType, target: TargetType) {
-        guard let httpRequest = request.request else {
+        guard let _ = request.request else {
             Log.error("유효하지 않은 요청입니다.")
             return
         }
-  
+        
+        /*
         let url = httpRequest.url?.description ?? ""
         let method = httpRequest.method?.rawValue ?? ""
         var log = "----------------------- ✨ API Log ✨ -----------------------\n"
@@ -39,6 +40,7 @@ final class MoyaLoggerPlugin: PluginType {
         }
         log.append("----------------------- ✨ End Log ✨ -----------------------")
         print(log)
+        */
     }
     
     func didReceive(_ result: Result<Response, MoyaError>, target: TargetType) {
@@ -57,12 +59,19 @@ private extension MoyaLoggerPlugin {
         let request = response.request
         let url = request?.url?.absoluteString ?? ""
         let statusCode = response.statusCode
+        let header = response.request?.allHTTPHeaderFields ?? [:]
+        let headerStr: String = header.map {
+            return "\n" + #"""# + "\($0.key)" + #"""# + ": " + #"""# + "\($0.value)" + #"""#
+        }.joined()
         
         let log = """
             ----------------------- ✨ API Log ✨ -----------------------
             [Did Receive - Success]
             ✨ API: \(target)
             ✨ URL: \(url)
+            ✨ METHOD: \(response.request?.method?.rawValue ?? "")
+            ✨ HEADER: \(headerStr)
+            ✨ BODY: \(String(bytes: response.request?.httpBody ?? Data(), encoding: String.Encoding.utf8) ?? "")
             ✨ STATUS CODE: \(statusCode)
             ----------------------- ✨ End Log ✨ -----------------------
             """
@@ -72,6 +81,7 @@ private extension MoyaLoggerPlugin {
         do {
             let message = try JSONDecoder().decode(ShortMessageResult.self, from: response.data)
             if UserInfoManager.isLoggedIn && !message.success && message.message == "로그인이 필요한 작업입니다." {
+                Log.error("🚨reissue🚨")
                 AuthManager.shared.reissueForPlugin()
             }
         } catch {
@@ -81,19 +91,24 @@ private extension MoyaLoggerPlugin {
     
     func onFail(_ error: MoyaError, target: TargetType) {
         
-        let request = error.response?.request
+        let response = error.response
+        let request = response?.request
         let url = request?.url?.absoluteString ?? ""
-        let statusCode = error.response?.statusCode ?? 0
-        let errorDescription = error.errorDescription ?? ""
+        let statusCode = response?.statusCode
+        let header = response?.request?.allHTTPHeaderFields ?? [:]
+        let headerStr: String = header.map {
+            return "\n" + #"""# + "\($0.key)" + #"""# + ": " + #"""# + "\($0.value)" + #"""#
+        }.joined()
         
         let log = """
             ----------------------- 🚨 API Log 🚨 -----------------------
-            [Did Receive - Failure]
+            [Did Receive - Success]
             🚨 API: \(target)
             🚨 URL: \(url)
-            🚨 STATUS CODE: \(statusCode)
-            🚨 ERROR: \(error)
-            🚨 ERROR Description: \(errorDescription)
+            🚨 METHOD: \(response?.request?.method?.rawValue ?? "")
+            🚨 HEADER: \(headerStr)
+            🚨 BODY: \(String(bytes: response?.request?.httpBody ?? Data(), encoding: String.Encoding.utf8) ?? "")
+            🚨 STATUS CODE: \(statusCode ?? -1)
             ----------------------- 🚨 End Log 🚨 -----------------------
             """
         print(log)
