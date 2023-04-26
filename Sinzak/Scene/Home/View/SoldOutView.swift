b714f40b14b965b3928cbbcfaed5e77638c55617
JongHooon <qnm541@gmail.com>
2023-04-26 22:32:57 +0900

    [fix] like view, sold out view layout 수정, sold out view 있을때는 like view 안보이게 수정

--- Sinzak/Scene/Home/View/SoldOutView.swift
+++ Sinzak/Scene/Home/View/SoldOutView.swift
@@ -58,13 +58,13 @@ final class SoldOutView: UIView {
         
         checkImageView.snp.makeConstraints {
             $0.centerX.equalToSuperview()
-            $0.top.equalToSuperview().inset(60.0)
-            $0.width.height.equalTo(24.0 * (24.0 / 17.5))
+            $0.top.equalToSuperview().inset(56.0)
+            $0.width.height.equalTo(32.0)
         }
         
         infoLabel.snp.makeConstraints {
             $0.centerX.equalToSuperview()
-            $0.top.equalTo(checkImageView.snp.bottom).offset(6.0)
+            $0.top.equalTo(checkImageView.snp.bottom)
         }
     }
     
