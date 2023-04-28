d0bb41f4dc120aa13ee60a2cb9453e539e097e03
JongHooon <qnm541@gmail.com>
2023-04-28 17:35:29 +0900

    [Refactor] See More cell - UI 수정

--- Sinzak/Scene/Home/View/SeeMoreCVC.swift
+++ Sinzak/Scene/Home/View/SeeMoreCVC.swift
@@ -11,9 +11,23 @@ import Then
 
 final class SeeMoreCVC: UICollectionViewCell {
     // MARK: - Properties
+
+    var alignOption: AlignOption? {
+        willSet {
+            guard let newValue = newValue else { return }
+            if newValue == .popular && UserInfoManager.isLoggedIn {
+                imageView.image = nil
+            } else {
+                imageView.image = UIImage(named: "moreCell")?
+                    .withTintColor(CustomColor.label!, renderingMode: .alwaysOriginal)
+            }
+        }
+    }
+    
     let imageView = UIImageView().then {
-        $0.contentMode = .scaleAspectFill
-        $0.image = UIImage(named: "moreCell")
+        $0.contentMode = .scaleAspectFit
+        $0.image = UIImage(named: "moreCell")?
+            .withTintColor(CustomColor.label!, renderingMode: .alwaysOriginal)
     }
     
     // MARK: - Init
@@ -30,11 +44,12 @@ final class SeeMoreCVC: UICollectionViewCell {
         contentView.backgroundColor = .clear
         contentView.addSubview(imageView)
     }
+    
     func setConstraints() {
         imageView.snp.makeConstraints { make in
-            make.leading.top.equalToSuperview()
-            make.width.equalTo(27)
-            make.height.equalTo(163)
+            make.leading.equalToSuperview()
+            make.top.equalToSuperview().inset(164/2 - 24/2)
+            make.width.height.equalTo(24.0)
         }
     }
 }
