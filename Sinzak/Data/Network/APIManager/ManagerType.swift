a685e9686b597a3ce3bda9590ca86c90e0256959
JongHooon <qnm541@gmail.com>
2023-05-02 15:35:55 +0900

    [Feature] ManagerType protocol 추가
    filterError - success false 로 주고 message 있는 경우 error throw
    
    getData - BaseDTO 사용하는 경우 data 받기

--- Sinzak/Data/Network/APIManager/ManagerType.swift
+++ Sinzak/Data/Network/APIManager/ManagerType.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import Moya
+
+protocol ManagerType {
+    associatedtype Target: TargetType
+    var provider: MoyaProvider<Target> { get }
+}
+
+extension ManagerType {
+    func filterError<T>(_ response: BaseDTO<T>) throws -> BaseDTO<T> {
+        if response.success {
+            return response
+        } else {
+            if let message = response.message {
+                throw APIError.errorMessage(message)
+            } else {
+                throw APIError.unknown(nil)
+            }
+        }
+    }
+    
+    func getData<T>(_ response: BaseDTO<T>) throws -> T {
+        guard let data = response.data else { throw APIError.noContent }
+        return data
+    }
+}
