3537ce483837a70748a87eda7cc2dd6c1fc103c7
JongHooon <qnm541@gmail.com>
2023-05-12 00:03:08 +0900

    [Feature] SelectIconCVC 추가

---
+++ Sinzak/Scene/MyProfile/SelectProfileIcon/View/SelectIconCVC.swift
@@ -0,0 +1,62 @@
+//
+//  SelectIconCVC.swift
+//  Sinzak
+//
+//  Created by JongHoon on 2023/05/11.
+//
+
+import UIKit
+
+final class SelectIconCVC: UICollectionViewCell {
+    
+    override var isSelected: Bool {
+        willSet {
+            switch newValue {
+            case true:
+                imageView.layer.borderColor = CustomColor.red.cgColor
+                imageView.layer.borderWidth = 2.0
+                
+            case false:
+                imageView.layer.borderColor = CustomColor.gray40.cgColor
+                
+            }
+        }
+    }
+    
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.borderWidth = 1.0
+        imageView.layer.borderColor = CustomColor.gray40.cgColor
+        imageView.layer.cornerRadius = 30.0
+        imageView.clipsToBounds = true
+        imageView.backgroundColor = .systemGray2
+        
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        
+        configLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configImage(with image: UIImage?) {
+        imageView.image = image
+    }
+}
+
+private extension SelectIconCVC {
+    
+    func configLayout() {
+        backgroundColor = CustomColor.background
+        contentView.addSubview(imageView)
+        
+        imageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
