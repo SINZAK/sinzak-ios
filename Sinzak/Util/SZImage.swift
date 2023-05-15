82e2920116a802c66093036a25e221a523900e39
JongHooon <qnm541@gmail.com>
2023-05-15 15:13:51 +0900

    [Feature] SZImage 추가

---
+++ Sinzak/Util/SZImage.swift
@@ -0,0 +1,17 @@
+//
+//  SZIcon.swift
+//  Sinzak
+//
+//  Created by JongHoon on 2023/05/15.
+//
+
+import UIKit
+
+struct SZImage {
+    
+    enum Icon {
+        static let dismiss = UIImage(named: "dismiss")?
+            .withTintColor(.label, renderingMode: .alwaysOriginal)
+    }
+    
+}
