d8a8231aaab8cbd8547d58c0d6e9b3581066f8f7
JongHooon <qnm541@gmail.com>
2023-05-18 04:35:24 +0900

    [Feature] setting cell feature 추가
    - 연결된 계정 표시
    - 앱 버전 표시
    - 문의 하기 구현
    - 공지사항, 서비스 약관, 개인정보 처리방침 웹뷰 연결
    - 회원탈퇴 화면 이동
    - 로그아웃 alert 표시

--- Sinzak/Scene/MyProfile/Setting/VC/SettingVC.swift
+++ Sinzak/Scene/MyProfile/Setting/VC/SettingVC.swift
@@ -9,6 +9,7 @@ import UIKit
 import NaverThirdPartyLogin
 import KakaoSDKUser
 import RxSwift
+import MessageUI
 
 final class SettingVC: SZVC {
     // MARK: - Properties
@@ -38,28 +39,37 @@ final class SettingVC: SZVC {
 extension SettingVC {
     private func createLayout() -> UICollectionViewLayout {
         var config = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
-        config.backgroundColor = CustomColor.white
+        config.backgroundColor = CustomColor.background
         config.headerMode = .supplementary
         let layout = UICollectionViewCompositionalLayout.list(using: config)
         return layout
     }
     private func configureDataSource() {
         // Cell Registration
-        let cellRegistration =  UICollectionView.CellRegistration<UICollectionViewListCell, String>(handler: { cell, indexPath, itemIdentifier in
-            var content = UIListContentConfiguration.valueCell()
-            content.textProperties.font = .body_R
-            // content.secondaryTextProperties.font = .body_R
-            content.text = itemIdentifier
-            // content.prefersSideBySideTextAndSecondaryText = false
-            // content.secondaryText = itemIdentifier
-            cell.contentConfiguration = content
-            // 백그라운드 설정 
-            var background = UIBackgroundConfiguration.listPlainCell()
-            background.strokeColor = CustomColor.gray40
-            background.strokeWidth = 1
-            background.cornerRadius = 20
-            cell.backgroundConfiguration = background
-        })
+        let cellRegistration =  UICollectionView.CellRegistration<UICollectionViewListCell, String>(
+            handler: { cell, indexPath, itemIdentifier in
+                var content = UIListContentConfiguration.valueCell()
+                content.textProperties.font = .body_R
+                 content.secondaryTextProperties.font = .body_R
+                content.text = itemIdentifier
+                // content.prefersSideBySideTextAndSecondaryText = false
+//                content.secondaryText = itemIdentifier
+                if itemIdentifier == "연결된 계정" {
+                    content.secondaryText = UserInfoManager.snsKind ?? ""
+                }
+                if itemIdentifier == "앱 버전" {
+                    content.secondaryText = "1.0.0"
+                }
+                
+                cell.contentConfiguration = content
+                // 백그라운드 설정
+                var background = UIBackgroundConfiguration.listPlainCell()
+                background.strokeColor = CustomColor.gray40
+                background.strokeWidth = 1
+                background.cornerRadius = 20
+                background.backgroundColor = CustomColor.background
+                cell.backgroundConfiguration = background
+            })
         // 헤더
         let headerRegistration = UICollectionView.SupplementaryRegistration
         <UICollectionViewListCell>(elementKind: UICollectionView.elementKindSectionHeader) {
@@ -71,7 +81,7 @@ extension SettingVC {
             configuration.text = headerItem.text
             // Customize header appearance to make it more eye-catching
             configuration.textProperties.font = .body_B
-            configuration.textProperties.color = CustomColor.black
+            configuration.textProperties.color = CustomColor.label
             configuration.directionalLayoutMargins = .init(top: 20.0, leading: 0.0, bottom: 14.0, trailing: 0.0)
             // Apply the configuration to header view
             headerView.contentConfiguration = configuration
@@ -80,7 +90,11 @@ extension SettingVC {
         // collectionView.dataSource = self 코드의 대체
         // CellForItemAt 대체
         dataSource = UICollectionViewDiffableDataSource(collectionView: mainView.collectionView, cellProvider: { collectionView, indexPath, itemIdentifier in
-            let cell = collectionView.dequeueConfiguredReusableCell(using: cellRegistration , for: indexPath, item: itemIdentifier)
+            let cell = collectionView.dequeueConfiguredReusableCell(
+                using: cellRegistration,
+                for: indexPath,
+                item: itemIdentifier
+            )
             return cell
         })
         dataSource.supplementaryViewProvider  = { [unowned self]
@@ -111,17 +125,63 @@ extension SettingVC: UICollectionViewDelegate {
         // 내용별로 하기
         collectionView.deselectItem(at: indexPath, animated: true)
         
-        if indexPath == [2, 0] { // 회원 탈퇴
-            signout()
-        }
+        switch indexPath {
+            
+        case [1, 1]:
+            sendInquiry()
+            
+        case [1, 2]:
+            let vc = WebVC()
+            vc.destinationURL = "https://sinzak.notion.site/64bb148cab2b49219a8d3d9309c80c87"
+            present(vc, animated: true)
         
-        if indexPath == [2, 1] { // 로그 아웃
+        case [1, 3]:
+            let vc = WebVC()
+            vc.destinationURL = "https://sinzak.notion.site/bfd66407b0ca4d428a8214165627c191"
+            present(vc, animated: true)
+            
+        case [1, 4]:
+            let vc = WebVC()
+            vc.destinationURL = "https://sinzak.notion.site/cd0047fcc1d1451aa0375eae9b60f5b4"
+            present(vc, animated: true)
+            
+        case [2, 0]:
+            let vc = SignoutCheckVC()
+            navigationController?.pushViewController(vc, animated: true)
+            
+        case [2, 1]:
+            showPopUpAlert(
+                message: "정말 로그아웃 할까요?",
+                rightActionTitle: "로그아웃",
+                rightActionCompletion: { [weak self] in
+                    self?.showLoading()
+                    self?.logout()
+                }
+            )
             logout()
+            
+        default:
+            break
         }
     }
 }
 
 private extension SettingVC {
+    
+    func sendInquiry() {
+        if MFMailComposeViewController.canSendMail() {
+            let composeViewController = MFMailComposeViewController()
+            composeViewController.mailComposeDelegate = self
+            
+            composeViewController.setToRecipients(["sinzakofficial@naver.com"])
+            composeViewController.setSubject("[신작 앱 1:1문의]")
+            
+            present(composeViewController, animated: true)
+        } else {
+            showSinglePopUpAlert(message: "sinzakofficial@naver.com\n로 문의해주세요.")
+        }
+        
+    }
     
     func logout() {
         let snsKind = SNS(rawValue: UserInfoManager.snsKind ?? "")
@@ -148,12 +208,10 @@ private extension SettingVC {
         UserInfoManager.shared.logout(completion: { [weak self] in
             let vc = TabBarVC()
             vc.modalPresentationStyle = .fullScreen
+            self?.hideLoading()
             self?.present(vc, animated: true, completion: {
                 self?.navigationController?.popToRootViewController(animated: false)
             })
-            
-//            (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?
-//                .changeRootVC(vc, animated: true)
         })
     }
     
@@ -192,14 +250,21 @@ private extension SettingVC {
                         owner.present(vc, animated: true, completion: {
                             owner.navigationController?.popToRootViewController(animated: false)                            
                         })
-//                        (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?
-//                            .changeRootVC(vc, animated: true)
-                        
                     })
                     
                 }, onFailure: { _, error in
                     APIError.logError(error)
                 })
             .disposed(by: disposeBag)
+    }
+}
+
+extension SettingVC: MFMailComposeViewControllerDelegate {
+    func mailComposeController(
+        _ controller: MFMailComposeViewController,
+        didFinishWith result: MFMailComposeResult,
+        error: Error?
+    ) {
+        dismiss(animated: true)
     }
 }
