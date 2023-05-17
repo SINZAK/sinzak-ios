6f3eed3d98068d305b92fd2d6e224a8d88331b32
JongHooon <qnm541@gmail.com>
2023-05-17 14:57:51 +0900

    [Feature] CertifySchoolCardDTO 추가

---
+++ Sinzak/Data/Network/DTO/CertifySchoolCardDTO.swift
@@ -0,0 +1,14 @@
+//
+//  CertifySchoolCardDTO.swift
+//  Sinzak
+//
+//  Created by JongHoon on 2023/05/16.
+//
+
+import Foundation
+
+struct CertifySchoolCardDTO: Codable {
+    let success: Bool?
+    let message: String?
+    let id: Int?
+}
