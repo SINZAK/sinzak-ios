596e95a90db0d9e3ce0051e18d48d9374438fbd0
JongHooon <qnm541@gmail.com>
2023-05-17 15:45:31 +0900

    [Feature] 이메일 인증, 학생증 인증 구현

--- Sinzak/Scene/SchoolAuth/StudentAuth/VC/StudentAuthVC.swift
+++ Sinzak/Scene/SchoolAuth/StudentAuth/VC/StudentAuthVC.swift
@@ -10,15 +10,29 @@ import YPImagePicker
 import RxSwift
 import RxCocoa
 import RxKeyboard
+import PhotosUI
 
 final class StudentAuthVC: SZVC {
     // MARK: - Properties
     private let mainView = StudentAuthView()
     var authType = SchoolAuthType.webmail
-    var viewModel = SchoolAuthViewModel()
+    var viewModel: StudentAuthVM
+    var mode: EditViewMode
     var schoolcardImage: UIImage? = UIImage()
     
     private let disposeBag = DisposeBag()
+    
+    // MARK: - Init
+    
+    init(viewModel: StudentAuthVM, mode: EditViewMode) {
+        self.viewModel = viewModel
+        self.mode = mode
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     // MARK: - Lifecycle
     override func loadView() {
@@ -46,33 +60,14 @@ final class StudentAuthVC: SZVC {
         mainView.schoolCardView.isHidden = false
         mainView.webmailView.isHidden = true
     }
-    @objc func doNextButtonTapped(_ sender: UIButton) {
-        let vc = WelcomeVC()
-        vc.modalPresentationStyle = .fullScreen
-        present(vc, animated: true)
-    }
-    @objc func finishButtonTapped(_ sender: UIButton) {
-        // 학생증 인증 API 보내기
-        switch authType {
-        case .webmail:
-            print("webmail")
-        case .idcard:
-            print("idcard")
-        }
-        // 웰컴화면으로 보내기
-        let vc = WelcomeVC()
-        vc.modalPresentationStyle = .fullScreen
-        present(vc, animated: true)
-    }
+
     // MARK: - Helpers
     override func configure() {
         mainView.webmailButton.addTarget(self, action: #selector(webmailButtonTapped), for: .touchUpInside)
         mainView.schoolcardButton.addTarget(self, action: #selector(schoolCardButtonTapped), for: .touchUpInside)
-        mainView.doNextButton.addTarget(self, action: #selector(doNextButtonTapped), for: .touchUpInside)
-        mainView.finishButton.addTarget(self, action: #selector(finishButtonTapped), for: .touchUpInside)
         
         let touch = UITapGestureRecognizer(target: self, action: #selector(endEditing))
-        mainView.containerView.addGestureRecognizer(touch)
+        view.addGestureRecognizer(touch)
         
         bind()
     }
@@ -81,6 +76,7 @@ final class StudentAuthVC: SZVC {
         navigationItem.title = I18NStrings.schoolAuth
     }
     func bind() {
+        /*
         // 사진 업로드
         mainView.photoUploadButton.rx
             .tap
@@ -167,9 +163,9 @@ final class StudentAuthVC: SZVC {
                 vc.mainView.layoutIfNeeded()
             }
             .disposed(by: viewModel.disposeBag)
+         */
 
-        
-        
+        /*
         RxKeyboard.instance.visibleHeight
             .skip(1)
             .drive(onNext: { [weak self] keyboardVisibleHeignt in
@@ -193,9 +189,9 @@ final class StudentAuthVC: SZVC {
 //                            make.top.equalTo(self.view.safeAreaLayoutGuide).inset(-32)
 //                        }
 //                        self.view.layoutIfNeeded()
-                    self.mainView.scrollView.contentInset.bottom = 100
-                    
-                    self.mainView.scrollView.scroll(to: .bottom)
+//                    self.mainView.scrollView.contentInset.bottom = 100
+//
+//                    self.mainView.scrollView.scroll(to: .bottom)
 //                    self.mainView.scrollView.
 
                 } else {
@@ -211,17 +207,286 @@ final class StudentAuthVC: SZVC {
                 }
             })
             .disposed(by: disposeBag)
+         */
         
         bindInput()
         bindOutput()
     }
     
     func bindInput() {
+        mainView.webmailTextField.rx.text
+            .orEmpty
+            .skip(1)
+            .bind(
+                with: self,
+                onNext: { owner, text in
+                    owner.viewModel.webMailTextInput(text: text)
+                }
+            )
+            .disposed(by: disposeBag)
         
+        mainView.webmailTextField.rx.controlEvent(.editingDidEndOnExit)
+            .bind(
+                with: self,
+                onNext: { owner, _ in
+                    guard owner.viewModel.webMailTextValidResult.value == .valid else { return }
+                    
+                    owner.viewModel.tranmitUnivMail()
+                }
+            )
+            .disposed(by: disposeBag)
+        
+        mainView.transmitMailButton.rx.tap
+            .bind(
+                with: self,
+                onNext: { owner, _ in
+                    owner.viewModel.tranmitUnivMail()
+                }
+            )
+            .disposed(by: disposeBag)
+        
+        let authCodeText =  mainView.authCodeTextField.rx.text
+            .orEmpty
+            .share()
+        
+        authCodeText
+            .map { $0.count == 4 && Int($0) != nil }
+            .asDriver(onErrorJustReturn: false)
+            .drive(mainView.confirmCodeButton.rx.isEnabled)
+            .disposed(by: disposeBag)
+        
+        authCodeText
+            .map { $0.count == 4 && Int($0) != nil }
+            .asSignal(onErrorJustReturn: false)
+            .emit(
+                with: self,
+                onNext: { owner, isValid in
+                    switch isValid {
+                    case true:
+                        owner.mainView.authCodeValidationLabel.text = ""
+                    case false:
+                        owner.mainView.authCodeValidationLabel.text = "다시 입력해주세요."
+                        owner.mainView.authCodeValidationLabel.textColor = CustomColor.red
+                    }
+            })
+            .disposed(by: disposeBag)
+        
+        mainView.authCodeTextField.rx.controlEvent(.editingDidEndOnExit)
+            .bind(
+                with: self,
+                onNext: { owner, _ in
+                    let code: Int = Int(owner.mainView.authCodeTextField.text ?? "") ?? 0
+                    owner.viewModel.certifyUnivEmailCode(code: code)
+                }
+            )
+            .disposed(by: disposeBag)
+
+        mainView.confirmCodeButton.rx.tap
+            .bind(
+                with: self,
+                onNext: { owner, _ in
+                    let code: Int = Int(owner.mainView.authCodeTextField.text ?? "") ?? 0
+                    owner.viewModel.certifyUnivEmailCode(code: code)
+                }
+            )
+            .disposed(by: disposeBag)
+        
+        mainView.photoUploadButton.rx.tap
+            .bind(
+                with: self,
+                onNext: { owner, _ in
+                    var configuration = PHPickerConfiguration()
+                    configuration.selectionLimit = 1
+                    configuration.filter = .images
+                    
+                    let picker = PHPickerViewController(configuration: configuration)
+                    picker.delegate = self
+                    picker.view.tintColor = CustomColor.red
+                    picker.modalPresentationStyle = .fullScreen
+                    
+                    owner.present(picker, animated: true)
+                })
+            .disposed(by: disposeBag)
+        
+        mainView.webMailDoNextButton.rx.tap
+            .bind(
+                with: self,
+                onNext: { owner, _ in
+                    switch owner.mode {
+                    case .signUp:
+                        let vc = WelcomeVC()
+                        vc.modalPresentationStyle = .fullScreen
+                        owner.present(vc, animated: true)
+                    case .editProfile:
+                        owner.dismiss(animated: true)
+                    }
+                }
+            )
+            .disposed(by: disposeBag)
+        
+        mainView.webMailFinishButton.rx.tap
+            .bind(
+                with: self,
+                onNext: { owner, _ in
+                    switch owner.mode {
+                    case .signUp:
+                        let vc = WelcomeVC()
+                        vc.modalPresentationStyle = .fullScreen
+                        owner.present(vc, animated: true)
+                    case .editProfile:
+                        owner.dismiss(animated: true)
+                    }
+                }
+            )
+            .disposed(by: disposeBag)
+        
+        mainView.schoolCardDoNextButton.rx.tap
+            .bind(
+                with: self,
+                onNext: { owner, _ in
+                    switch owner.mode {
+                    case .signUp:
+                        let vc = WelcomeVC()
+                        vc.modalPresentationStyle = .fullScreen
+                        owner.present(vc, animated: true)
+                    case .editProfile:
+                        owner.dismiss(animated: true)
+                    }
+                }
+            )
+            .disposed(by: disposeBag)
+        
+        mainView.schoolCardFinishButton.rx.tap
+            .bind(
+                with: self,
+                onNext: { owner, _ in
+                    
+                    let image = owner.mainView.uploadedImageView.image ?? UIImage()
+                    owner.showLoading()
+                    owner.viewModel.transmitSchoolCar(image: image)
+                    
+                }
+            )
+            .disposed(by: disposeBag)
     }
     
     func bindOutput() {
         
+        let webMailTextValidResult = viewModel.webMailTextValidResult
+            .skip(1)
+            .asDriver(onErrorJustReturn: .notValid)
+        
+        webMailTextValidResult
+            .drive(with: self, onNext: { owner, result in
+                owner.mainView.webmailValidationLabel.text = result.info
+                owner.mainView.webmailValidationLabel.textColor = result.color
+            })
+            .disposed(by: disposeBag)
+        
+        webMailTextValidResult
+            .map { $0 == .valid }
+            .drive(mainView.transmitMailButton.rx.isEnabled)
+            .disposed(by: disposeBag)
+        
+        viewModel.errorHandler
+            .subscribe(
+                with: self,
+                onNext: { owner, error in
+                    owner.simpleErrorHandler.accept(error)
+            })
+            .disposed(by: disposeBag)
+        
+        viewModel.showAuthCodeView
+            .asDriver(onErrorJustReturn: false)
+            .drive(with: self, onNext: { owner, _ in
+                owner.mainView.webmailTextField.isEnabled = false
+                owner.viewModel.webMailTextValidResult.accept(.authComplete)
+                
+                UIView.animate(
+                    withDuration: 0.5,
+                    delay: .zero,
+                    animations: {
+                        [
+                            owner.mainView.authCodeLabel,
+                            owner.mainView.authCodeTextField,
+                            owner.mainView.confirmCodeButton,
+                            owner.mainView.authCodeValidationLabel
+                        ].forEach {
+                            $0.isHidden = false
+                            $0.alpha = 1
+                        }
+                    })
+                
+                UIView.transition(
+                    with: owner.mainView.authCodeLabel,
+                    duration: 0.5,
+                    animations: {
+                        owner.mainView.authCodeLabel.textColor = CustomColor.label.withAlphaComponent(1)
+                    }
+                )
+                
+            })
+            .disposed(by: disposeBag)
+        
+        viewModel.completeEmailCertify
+            .asSignal(onErrorJustReturn: false)
+            .emit(
+                with: self,
+                onNext: { owner, _ in
+                    owner.mainView.authCodeTextField.isEnabled = false
+                    owner.mainView.confirmCodeButton.isEnabled = false
+                    owner.mainView.authCodeValidationLabel.textColor = CustomColor.purple
+                    owner.mainView.authCodeValidationLabel.text = "인증 완료!"
+                    owner.mainView.webMailFinishButton.isEnabled = true
+                }
+            )
+            .disposed(by: disposeBag)
+        
+        viewModel.completeSchoolCardCertify
+            .asSignal()
+            .emit(
+                with: self,
+                onNext: { owner, _ in
+                    owner.hideLoading()
+                    switch owner.mode {
+                    case .signUp:
+                        let vc = WelcomeVC()
+                        vc.modalPresentationStyle = .fullScreen
+                        owner.present(vc, animated: true)
+                    case .editProfile:
+                        owner.dismiss(animated: true)
+                    }
+            })
+            .disposed(by: disposeBag)
+    }
+}
+
+// MARK: - PHPicker
+extension StudentAuthVC: PHPickerViewControllerDelegate {
+    func picker(
+        _ picker: PHPickerViewController,
+        didFinishPicking results: [PHPickerResult]
+    ) {
+        
+        dismiss(animated: true)
+        
+        let itemProvider = results.first?.itemProvider
+        
+        if let itemProvider = itemProvider, itemProvider.canLoadObject(ofClass: UIImage.self) {
+            itemProvider.loadObject(ofClass: UIImage.self, completionHandler: { image, error in
+                if let error = error {
+                    Log.error(error)
+                    return
+                }
+                
+                if let image = image as? UIImage {
+                    DispatchQueue.main.async { [weak self] in
+                        self?.mainView.uploadedImageView.image = image
+                        self?.mainView.schoolCardFinishButton.isEnabled = true
+                    }
+                }
+            })
+        }
     }
 }
 
