b6156995339147e9e7a8b70e1de11443ec2d22c5
JongHooon <qnm541@gmail.com>
2023-05-23 17:22:40 +0900

    [Feature] 글작성 완료 구현

--- Sinzak/Scene/Write/WritePost/VM/WritePostVM.swift
+++ Sinzak/Scene/Write/WritePost/VM/WritePostVM.swift
@@ -16,11 +16,30 @@ protocol WritePostVMInput {
     
     func photoSelected(images: [UIImage])
     func deleteImage(image: UIImage)
+    
+    func postProductsComplete(
+        title: String,
+        price: Int,
+        suggest: Bool,
+        body: String,
+        width: Int,
+        vertical: Int,
+        heigth: Int
+    )
+    
+    func postWorksComplete(
+        title: String,
+        price: Int,
+        suggest: Bool,
+        body: String
+    )
 }
 
 protocol WritePostVMOutput {
     
     var photoSections: BehaviorRelay<[PhotoSection]> { get }
+    
+    var needDismiss: PublishRelay<Bool> { get }
     
 }
 
@@ -57,10 +76,87 @@ final class DefaultAddPhotosVM: WritePostVM {
         photoSections.accept([section])
     }
     
+    func postProductsComplete(
+        title: String,
+        price: Int,
+        suggest: Bool,
+        body: String,
+        width: Int,
+        vertical: Int,
+        heigth: Int
+    ) {
+        
+        let marketBuild = MarketBuild(
+            category: selectedGenres.joined(separator: ","),
+            title: title,
+            content: body,
+            height: heigth,
+            vertical: vertical,
+            width: width,
+            suggest: suggest,
+            price: price
+        )
+        
+        let images: [UIImage] = photoSections.value[0].items
+        
+        ProductsManager.shared.buildProductsPost(
+            products: marketBuild,
+            images: images
+        )
+        .subscribe(
+            with: self,
+            onSuccess: { owner, _ in
+                owner.compltePost()
+            },
+            onFailure: { _, error in
+                Log.error(error)
+            })
+        .disposed(by: disposeBag)
+    }
+    
+    func postWorksComplete(
+        title: String,
+        price: Int,
+        suggest: Bool,
+        body: String
+    ) {
+        let worksBuild = WorkBuild(
+            category: selectedGenres.joined(separator: ","),
+            title: title,
+            content: body,
+            employment: selectedCategory == .request,
+            price: price,
+            suggest: suggest
+        )
+        
+        let images: [UIImage] = photoSections.value[0].items
+        WorksManager.shared.buildWorksPost(works: worksBuild, images: images)
+            .subscribe(
+                with: self,
+                onSuccess: { owner, _ in
+                    owner.compltePost()
+                },
+                onFailure: { _, error in
+                    Log.error(error)
+                })
+            .disposed(by: disposeBag)
+        
+    }
+    
     // MARK: - Output
     
     var photoSections: BehaviorRelay<[PhotoSection]> = .init(
         value: [PhotoSection(model: "", items: [UIImage()])]
     )
+    
+    var needDismiss: PublishRelay<Bool> = .init()
+}
+
+private extension DefaultAddPhotosVM {
+    
+    func compltePost() {
+        NotificationCenter.default.post(name: .completePost, object: selectedCategory)
+        needDismiss.accept(true)
+    }
     
 }
