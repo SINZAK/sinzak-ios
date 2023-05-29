44642bee811554344b3a57051759eecb74bce1e8
JongHooon <qnm541@gmail.com>
2023-05-30 02:14:33 +0900

    [Chore] NSNotification-Name+Extension.swift 디렉토리 이동

---
+++ Sinzak/Global/Extension/NSNotification-Name+Extension.swift
@@ -0,0 +1,17 @@
+//
+//  NSNotification-Name+Extension.swift
+//  Sinzak
+//
+//  Created by JongHoon on 2023/04/26.
+//
+
+import Foundation
+
+extension NSNotification.Name {
+    static let productsCellLikeUpdate = NSNotification.Name("productsCellLikeUpdate")
+    static let worksCellLikeUpdate = NSNotification.Name("worksCellLikeUpdate")
+    static let completePost = NSNotification.Name("completePost")
+    
+    static let goLogin = NSNotification.Name("goLogin")
+//    static let cellIsCompleteUpdate = NSNotification.Name("cellIsCompleteUpdate")
+}
