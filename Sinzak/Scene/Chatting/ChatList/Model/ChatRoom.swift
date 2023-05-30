4497da295597d57b479993a98045dc2554b5c9ee
JongHooon <qnm541@gmail.com>
2023-05-30 12:37:45 +0900

    [Feature] ChatRoom 추가

---
+++ Sinzak/Scene/Chatting/ChatList/Model/ChatRoom.swift
@@ -0,0 +1,17 @@
+//
+//  ChatRoom.swift
+//  Sinzak
+//
+//  Created by JongHoon on 2023/05/28.
+//
+
+import Foundation
+
+struct ChatRoom {
+    let roomUUID: String
+    let image: String
+    let roomName: String
+    let univ: String
+    let latestMessage: String
+    let latestMessageTime: String
+}
