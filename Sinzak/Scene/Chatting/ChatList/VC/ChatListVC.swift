fe849f54e6426c6579bfc80431b22d00671e7d4f
JongHooon <qnm541@gmail.com>
2023-05-30 12:44:11 +0900

    [Feature] viewModel 추가, fetchChatList 연결, RxDataSource로 전환

--- Sinzak/Scene/Chatting/ChatList/VC/ChatListVC.swift
+++ Sinzak/Scene/Chatting/ChatList/VC/ChatListVC.swift
@@ -6,11 +6,29 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import RxDataSources
 
 final class ChatListVC: SZVC {
     // MARK: - Properties
     let mainView = ChatListView()
+    let viewModel: ChatListVM
+    let disposeBag = DisposeBag()
     // MARK: - Init
+    
+    init(viewModel: ChatListVM) {
+        self.viewModel = viewModel
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Lifecycle
+    
     override func loadView() {
         view = mainView
     }
@@ -20,38 +38,93 @@ final class ChatListVC: SZVC {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         tabBarController?.tabBar.isHidden = false
+        
+        viewModel.fetchChatList()
     }
     // MARK: - Helper
     override func setNavigationBar() {
-        navigationItem.title = "채팅"
+        let leftTitle = UIBarButtonItem(
+            title: "채팅",
+            style: .plain,
+            target: nil,
+            action: nil
+        )
+        leftTitle.setTitleTextAttributes(
+            [
+                .font: UIFont.subtitle_B,
+                .foregroundColor: CustomColor.label
+            ],
+            for: .disabled
+        )
+        leftTitle.isEnabled = false
+        navigationItem.leftBarButtonItem = leftTitle
     }
     override func configure() {
-        mainView.collectionView.delegate = self
-        mainView.collectionView.dataSource = self
         mainView.collectionView.register(ChatListCVC.self, forCellWithReuseIdentifier: String(describing: ChatListCVC.self))
         mainView.collectionView.collectionViewLayout = createLayout()
+        bind()
     }
 }
-extension ChatListVC: UICollectionViewDelegate, UICollectionViewDataSource {
-    // 아이템 갯수
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+
+// MARK: - Bind
+
+extension ChatListVC {
+    
+    func bind() {
+        bindInput()
+        bindOutput()
     }
-    // 셀 구성
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: ChatListCVC.self), for: indexPath) as? ChatListCVC else { return UICollectionViewCell() }
-        cell.verifiedBadge.isHidden = indexPath.item % 3 < 1 ? true : false
-        cell.chatCountBackground.isHidden = indexPath.item % 3 > 0 ? true : false
-        return cell
+    
+    func bindInput() {
+        
+        mainView.collectionView.rx.modelSelected(ChatRoom.self)
+            .asSignal()
+            .emit(
+                with: self,
+                onNext: { owner, _ in
+                    let vc = ChatVC()
+                    //        // vc에 대화정보 전달
+                    //        // 소켓통신 열기
+                    owner.navigationController?.pushViewController(vc, animated: true)
+                })
+            .disposed(by: disposeBag)
     }
-    // 셀 클릭했을 때
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let vc = ChatVC()
-        // vc에 대화정보 전달
-        // 소켓통신 열기
-        navigationController?.pushViewController(vc, animated: true)
+    
+    func bindOutput() {
+        
+        let chatRoomsDataSource = getChatRoomsDataSource()
+        let chatRoomSectionModel = viewModel.chatRoomSectionModel
+            .asDriver()
+        
+        chatRoomSectionModel
+            .skip(1)
+            .map { !$0[0].items.isEmpty }
+            .drive(mainView.noContentsLabel.rx.isHidden)
+            .disposed(by: disposeBag)
+        
+        chatRoomSectionModel
+            .drive(mainView.collectionView.rx.items(dataSource: chatRoomsDataSource))
+            .disposed(by: disposeBag)
     }
+    
 }
+
+// MARK: - Data Source
+
+extension ChatListVC {
+    func getChatRoomsDataSource() -> RxCollectionViewSectionedReloadDataSource<ChatRoomSectionModel> {
+        return RxCollectionViewSectionedReloadDataSource<ChatRoomSectionModel>(configureCell: { _, collectionView, indexPath, item in
+            
+            let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: ChatListCVC.identifier,
+                for: indexPath
+            ) as! ChatListCVC
+            
+            cell.setCellInfo(chatRoom: item)
+            return cell
+        })}
+}
+
 // 컴포지셔널 레이아웃
 extension ChatListVC {
     /// 컴포지셔널 레이아웃 세팅
