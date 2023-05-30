dd8147b329d27eb4e065c6194a2e171a8f0b1e27
JongHooon <qnm541@gmail.com>
2023-05-30 12:44:46 +0900

    [Feature] fetchChatList, chatRoomSectionModel 추가

--- Sinzak/Scene/Chatting/ChatList/VC/ChatListVM.swift
+++ Sinzak/Scene/Chatting/ChatList/VC/ChatListVM.swift
@@ -10,12 +10,14 @@ import RxSwift
 import RxCocoa
 import RxDataSources
 
+typealias ChatRoomSectionModel = SectionModel<Void, ChatRoom>
+
 protocol ChatListVMInput {
-    
+    func fetchChatList()
 }
 
 protocol ChatListVMOutput {
-    
+    var chatRoomSectionModel: BehaviorRelay<[ChatRoomSectionModel]> { get }
 }
 
 protocol ChatListVM: ChatListVMInput, ChatListVMOutput {}
@@ -25,7 +27,22 @@ final class DefaultChatListVM: ChatListVM {
     private let disposeBag = DisposeBag()
     
     // MARK: - Input
+    func fetchChatList() {
+        ChatManager.shared.fetchChatList()
+            .subscribe(
+                with: self,
+                onSuccess: { owner, chatRooms in
+                    let chatRoomSectionModel = ChatRoomSectionModel(
+                        model: Void(),
+                        items: chatRooms
+                    )
+                    owner.chatRoomSectionModel.accept([chatRoomSectionModel])
+                })
+            .disposed(by: disposeBag)
+    }
     
     // MARK: - Output
+    
+    var chatRoomSectionModel: BehaviorRelay<[ChatRoomSectionModel]> = .init(value: [])
     
 }
