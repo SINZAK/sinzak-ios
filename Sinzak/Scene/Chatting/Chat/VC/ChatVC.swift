ac36149f9e157d791b939f37b68e326a3fa0c8c1
JongHooon <qnm541@gmail.com>
2023-05-30 15:51:49 +0900

    [Feature] keyboard event 처리, collectionView layout 수정, rightBarButtonItem tap 수정

--- Sinzak/Scene/Chatting/Chat/VC/ChatVC.swift
+++ Sinzak/Scene/Chatting/Chat/VC/ChatVC.swift
@@ -6,19 +6,63 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import RxKeyboard
 
 final class ChatVC: SZVC {
     // MARK: - Properties
+    
+    let disposeBag = DisposeBag()
+    
     let mainView = ChatView()
-    let dummyChat: [String] = [
-        "안녕하세요",
-        "판매중 맞으실까요?",
-        "앗, 안녕하세요!",
-        "넵 판매중 맞습니다 :)",
-        "직거래 괜찮으신가요?",
-        "직거래하기 너무 클까요?",
-        "네~ 사이즈 써 놓으신 거 보시면",
-        "딱 그 크기인 캔버스 입니다ㅎㅎ"
+    let dummyChat: [[String]] = [
+        [
+            "안녕하세요",
+            "판매중 맞으실까요?",
+            "앗, 안녕하세요!",
+            "넵 판매중 맞습니다 :)",
+            "직거래 괜찮으신가요?",
+            "직거래하기 너무 클까요?",
+            "네~ 사이즈 써 놓으신 거 보시면",
+            "딱 그 크기인 캔버스 입니다ㅎㅎ",
+            "딱 그 크기인 캔버스 입니다ㅎㅎ",
+            "딱 그 크기인 캔버스 입니다ㅎㅎ",
+            "네~ 사이즈 써 놓으신 거 보시면",
+            "딱 그 크기인 캔버스 입니다ㅎㅎ",
+            "딱 그 크기인 캔버스 입니다ㅎㅎ",
+        ],
+        [
+            "딱 그 크기인 캔버스 입니다ㅎㅎ",
+            "네~ 사이즈 써 놓으신 거 보시면",
+            "딱 그 크기인 캔버스 입니다ㅎㅎ",
+            "딱 그 크기인 캔버스 입니다ㅎㅎ",
+            "딱 그 크기인 캔버스 입니다ㅎㅎ",
+            "네~ 사이즈 써 놓으신 거 보시면",
+            "딱 그 크기인 캔버스 입니다ㅎㅎ",
+            "딱 그 크기인 캔버스 입니다ㅎㅎ",
+            "딱 그 크기인 캔버스 입니다ㅎㅎ",
+            "딱 그 크기인 캔버스 입니다ㅎㅎ",
+            "딱 그 크기인 캔버스 입니다ㅎㅎ",
+            "네~ 사이즈 써 놓으신 거 보시면",
+            "딱 그 크기인 캔버스 입니다ㅎㅎ",
+            "딱 그 크기인 캔버스 입니다ㅎㅎ",
+            "딱 그 크기인 캔버스 입니다ㅎㅎ",
+            "네~ 사이즈 써 놓으신 거 보시면",
+            "딱 그 크기인 캔버스 입니다ㅎㅎ",
+            "딱 그 크기인 캔버스 입니다ㅎㅎ",
+            "딱 그 크기인 캔버스 입니다ㅎㅎ",
+            "딱 그 크기인 캔버스 입니다ㅎㅎ",
+            "딱 그 크기인 캔버스 입니다ㅎㅎ네~ 사이즈 써 놓으신 거 보시면네~ 사이즈 써 놓으신 거 보시면네~ 사이즈 써 놓으신 거 보시면네~ 사이즈 써 놓으신 거 보시면",
+            "네~ 사이즈 써 놓으신 거 보시면네~ 사이즈 써 놓으신 거 보시면네~ 사이즈 써 놓으신 거 보시면네~ 사이즈 써 놓으신 거 보시면네~ 사이즈 써 놓으신 거 보시면네~ 사이즈 써 놓으신 거 보시면네~ 사이즈 써 놓으신 거 보시면네~ 사이즈 써 놓으신 거 보시면네~ 사이즈 써 놓으신 거 보시면네~ 사이즈 써 놓으신 거 보시면네~ 사이즈 써 놓으신 거 보시면네~ 사이즈 써 놓으신 거 보시면네~ 사이즈 써 놓으신 거 보시면네~ 사이즈 써 놓으신 거 보시면네~ 사이즈 써 놓으신 거 보시면네~ 사이즈 써 놓으신 거 보시면네~ 사이즈 써 놓으신 거 보시면",
+            "딱 그 크기인 캔버",
+            "딱 그 크기인 캔",
+            "딱 그 크기인g",
+            "네~ 사이즈",
+            "크기인",
+            "딱",
+            "딱그",
+        ]
     ]
     // MARK: - Init
     override func loadView() {
@@ -31,25 +75,7 @@ final class ChatVC: SZVC {
         super.viewWillAppear(animated)
         tabBarController?.tabBar.isHidden = true
     }
-    // MARK: - Actions
-    @objc func chatMenuButtonTapped(_ sender: UIBarButtonItem) {
-        // 액션시트 설정
-        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
-        let finished = UIAlertAction(title: "거래완료", style: .default) { action in
-            // 거래완료 액션
-        }
-        let isDealing = UIAlertAction(title: "거래중", style: .default) { action in
-            // 거래중 액션
-        }
-        let cancel = UIAlertAction(title: "취소", style: .cancel)
-        alert.addAction(finished)
-        alert.addAction(isDealing)
-        alert.addAction(cancel)
-        // 액션시트 색상
-        alert.view.tintColor = CustomColor.black
-        // 액션시트 띄우기
-        present(alert, animated: true)
-    }
+   
     // MARK: - Helpers
     override func setNavigationBar() {
         super.setNavigationBar()
@@ -57,32 +83,90 @@ final class ChatVC: SZVC {
         let chatMenu = UIBarButtonItem(
             image: UIImage(named: "chatMenu"),
             style: .plain,
-            target: self,
-            action: #selector(chatMenuButtonTapped))
+            target: nil,
+            action: nil)
         navigationItem.rightBarButtonItem = chatMenu
     }
     override func configure() {
         mainView.collectionView.delegate = self
         mainView.collectionView.dataSource = self
-        mainView.collectionView.collectionViewLayout = createLayout()
+        mainView.collectionView.collectionViewLayout = configLayout()
         mainView.collectionView.register(MyChatBubbleCVC.self, forCellWithReuseIdentifier: String(describing: MyChatBubbleCVC.self))
         mainView.collectionView.register(OtherChatBubbleCVC.self, forCellWithReuseIdentifier: String(describing: OtherChatBubbleCVC.self))
+        
+        bind()
     }
 }
+
+// MARK: - Bind
+private extension ChatVC {
+    func bind() {
+        bindInput()
+        bindOutput()
+    }
+    
+    func bindInput() {
+        
+        let tapViewGesture = UITapGestureRecognizer(target: nil, action: nil)
+        view.addGestureRecognizer(tapViewGesture)
+        tapViewGesture.rx.event
+            .asSignal(onErrorRecover: { _ in return .never() })
+            .emit(
+                with: self,
+                onNext: { owner, _ in
+                    owner.view.endEditing(true)
+            })
+            .disposed(by: disposeBag)
+        
+        RxKeyboard.instance.visibleHeight
+            .drive(
+                with: self,
+                onNext: { owner, keyboardHeight in
+                    if keyboardHeight > 0 {
+                        owner.mainView.setShowKeyboardLayout(height: keyboardHeight)
+                    } else {
+                        owner.mainView.setHideKeyboardLayout()
+                    }
+                    owner.view.layoutIfNeeded()
+                })
+            .disposed(by: disposeBag)
+        
+        navigationItem.rightBarButtonItem?.rx.tap
+            .bind(
+                with: self,
+                onNext: { owner, _ in
+                
+                    owner.showDoubleAlertSheet(
+                        firstActionText: "신고하기",
+                        secondActionText: "채팅방 나가기"
+                    )
+                })
+            .disposed(by: disposeBag)
+    }
+    
+    func bindOutput() {
+    
+    }
+}
+
 // 콜렉션 뷰 설정
 extension ChatVC: UICollectionViewDelegate, UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
         return dummyChat.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return dummyChat[section].count
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if indexPath.item % 3 == 0 {
-            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: OtherChatBubbleCVC.self), for: indexPath) as? OtherChatBubbleCVC else { return UICollectionViewCell() }
-            cell.chatLabel.text = dummyChat[indexPath.item]
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: MyChatBubbleCVC.self), for: indexPath) as? MyChatBubbleCVC else { return UICollectionViewCell() }
+            cell.chatLabel.text = dummyChat[indexPath.section][indexPath.item]
             cell.dateLabel.text = "오전 10:02"
             return cell
         } else {
-            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: MyChatBubbleCVC.self), for: indexPath) as? MyChatBubbleCVC else { return UICollectionViewCell() }
-            cell.chatLabel.text = dummyChat[indexPath.item]
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: OtherChatBubbleCVC.self), for: indexPath) as? OtherChatBubbleCVC else { return UICollectionViewCell() }
+            cell.chatLabel.text = dummyChat[indexPath.section][indexPath.item]
             cell.dateLabel.text = "오전 10:02"
             return cell
         }
@@ -95,7 +179,25 @@ extension ChatVC {
     private func createLayout() -> UICollectionViewLayout {
         var config = UICollectionLayoutListConfiguration(appearance: .plain)
         config.separatorConfiguration.color = .clear
+        config.backgroundColor = CustomColor.background
         let layout = UICollectionViewCompositionalLayout.list(using: config)
         return layout
+    }
+    
+    private func configLayout() -> UICollectionViewCompositionalLayout {
+        return UICollectionViewCompositionalLayout { _, layoutEnvironment in
+            var configuration = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
+            configuration.separatorConfiguration.color = .clear
+            configuration.backgroundColor = CustomColor.background
+            let section = NSCollectionLayoutSection.list(using: configuration, layoutEnvironment: layoutEnvironment)
+            section.contentInsets = NSDirectionalEdgeInsets(
+                top: 0,
+                leading: 0,
+                bottom: 6.0,
+                trailing: 0
+            )
+                        
+            return section
+        }
     }
 }
