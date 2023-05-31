872c1b23e24b14638bdc62fc732e154953c14c09
JongHooon <qnm541@gmail.com>
2023-05-31 22:15:37 +0900

    [Feature] ChatRoomInfo 추가

---
+++ Sinzak/Scene/Chatting/ChatList/Model/ChatRoomInfo.swift
@@ -0,0 +1,21 @@
+//
+//  ChatRoomInfo.swift
+//  Sinzak
+//
+//  Created by JongHoon on 2023/05/30.
+//
+
+import Foundation
+
+struct ChatRoomInfo {
+    var roomName: String
+    var postId: Int?
+    var postName: String
+    var price: Int
+    var thumbnail: String
+    var complete: Bool
+    var suggest: Bool
+    var postUserId: Int
+    var postType: String
+    var opponentUserId: Int
+}
