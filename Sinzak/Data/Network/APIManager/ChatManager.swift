d48c3832a88cdab908aff3d175b8937ce8532e1b
JongHooon <qnm541@gmail.com>
2023-05-31 22:17:02 +0900

    [Feature] getChatRoomInfo, getChatRoomMessage, uploadImage 추가

--- Sinzak/Data/Network/APIManager/ChatManager.swift
+++ Sinzak/Data/Network/APIManager/ChatManager.swift
@@ -5,7 +5,7 @@
 //  Created by JongHoon on 2023/05/28.
 //
 
-import Foundation
+import UIKit
 import Moya
 import RxSwift
 
@@ -26,5 +26,45 @@ final class ChatManager: ManagerType {
             .map(filterError)
             .map(getData)
             .map { $0.map { $0.toDomain() } }
+    }
+    
+    func getChatRoomInfo(roomID: String) -> Single<ChatRoomInfo> {
+        return provider.rx.request(.chatRoom(uuid: roomID))
+            .filterSuccessfulStatusCodes()
+            .map(BaseDTO<ChatRoomInfoDTO>.self)
+            .map(filterError)
+            .map(getData)
+            .map { $0.toDomain() }
+    }
+    
+    func getChatRoomMessage(roomID: String, page: Int) -> Single<[ChatMessage]> {
+        return provider.rx.request(.chatRoomMessages(
+            uuid: roomID,
+            page: page
+        ))
+        .filterSuccessfulStatusCodes()
+        .map(ChatMessageContentDTO.self)
+        .map { chatMessageContentDTO in
+            if chatMessageContentDTO.success == false {
+                Log.error(chatMessageContentDTO.message ?? "")
+                throw APIError.errorMessage(chatMessageContentDTO.message ?? "")
+            }
+            
+            let content = chatMessageContentDTO.content ?? []
+            let messages = content.map { $0.toDomain() }
+            
+            return messages
+        }
+    }
+    
+    func uploadImage(roomID: String, images: [UIImage]) -> Single<[String]> {
+        return provider.rx.request(.chatRoomImageUpload(uuid: roomID, images: images))
+            .filterSuccessfulStatusCodes()
+            .map(BaseDTO<[URLDTO]>.self)
+            .map(filterError)
+            .map(getData)
+            .map { urls in
+                return urls.map { $0.url }
+            }
     }
 }
