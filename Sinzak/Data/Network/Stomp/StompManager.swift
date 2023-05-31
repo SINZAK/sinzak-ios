73f95f8437392b752925541996b510575f2485aa
JongHooon <qnm541@gmail.com>
2023-05-31 22:05:28 +0900

    [Refactor] StompManager 수정

--- Sinzak/Data/Network/Stomp/StompManager.swift
+++ Sinzak/Data/Network/Stomp/StompManager.swift
@@ -11,44 +11,53 @@ import StompClientLib
 class StompManager {
     private init() {}
     static let shared: StompManager = StompManager()
-    
+
     private let url = URL(string: Endpoint.wsURL)
     var socketClient = StompClientLib()
     let header = [
-        "Authorization": KeychainItem.currentAccessToken // 키체인에서 불러온 Auth 토큰
+        "Authorization": KeychainItem.currentAccessToken, // 키체인에서 불러온 Auth 토큰
+        "hear-beat": "0,30000"
     ]
-    
+
     /// 소켓 연결
     func registerSocket() {
         socketClient.openSocketWithURLRequest(
-            request: NSURLRequest(url: url! as URL),
+            request: NSURLRequest(url: url!),
             delegate: self as StompClientLibDelegate,
             connectionHeaders: header
         )
     }
-    
+
     /// 소켓 연결 해제
     func disconnect() {
         if socketClient.isConnected() {
             socketClient.disconnect()
         }
     }
-    
-    func subscribe(roomId: String) {
-        let destination = "/sub/chat/room/\(roomId)"
+
+    func subscribe(roomID: String) {
+        let destination = "/sub/chat/rooms/\(roomID)"
         if socketClient.isConnected() {
+            Log.debug("Stomp subscribe \(roomID)")
             socketClient.subscribe(destination: destination)
         }
     }
-    
-    /// 메시지 전송
-    func sendMessage(message: Message) {
-        socketClient.sendJSONForDict(
-            dict: message.nsDictionary,
-            toDestination: "/pub/chat/message"
-        )
+
+    func unsubscribe(roomID: String) {
+        let destination = "/sub/chat/rooms/\(roomID)"
+        if socketClient.isConnected() {
+            socketClient.unsubscribe(destination: destination)
+        }
     }
-    
+
+    /// 메시지 전송
+//    func sendMessage(message: Message) {
+//        socketClient.sendJSONForDict(
+//            dict: message.nsDictionary,
+//            toDestination: "/pub/chat/message"
+//        )
+//    }
+
     func leaveChatroom(roomId: String) {
         let leave = Leave(roomId: roomId)
         socketClient.sendJSONForDict(
@@ -56,10 +65,22 @@ class StompManager {
             toDestination: "/pub/chat/leave"
         )
     }
-    
+
+    func sendPing() {
+
+    }
+
 }
 
 extension StompManager: StompClientLibDelegate {
+    func stompClientDidConnect(client: StompClientLib!) {
+        Log.debug("Stomp is connected successfully!")
+    }
+
+    func stompClientDidDisconnect(client: StompClientLib!) {
+        Log.debug("Stomp is Disconnected")
+    }
+
     func stompClient(
         client: StompClientLib!,
         didReceiveMessageWithJSONBody jsonBody: AnyObject?,
@@ -67,45 +88,42 @@ extension StompManager: StompClientLibDelegate {
         withHeader header: [String: String]?,
         withDestination destination: String
     ) {
-        print("DESTINATION : \(destination)")
-        print("JSON BODY : \(String(describing: jsonBody))")
-        print("STRING BODY : \(stringBody ?? "nil")")
+        print("DESTINATION: \(destination)")
+        print("JSON BODY: \(String(describing: jsonBody))")
+        print("STRING BODY: \(stringBody ?? "nil")")
+
+        if let data = stringBody?.data(using: String.Encoding.utf8) {
+            let message = try? JSONDecoder().decode(ChatMessageDTO.self, from: data)
+            Log.debug(message?.toDomain())
+        }
+
     }
-    
+
     func stompClientJSONBody(
         client: StompClientLib!,
         didReceiveMessageWithJSONBody jsonBody: String?,
-        withHeader header: [String : String]?,
+        withHeader header: [String: String]?,
         withDestination destination: String
     ) {
         print("DESTINATION : \(destination)")
         print("String JSON BODY : \(String(describing: jsonBody))")
     }
-    
-    func stompClientDidDisconnect(client: StompClientLib!) {
-        print("Socket is Disconnected")
-    }
-    
-    func stompClientDidConnect(client: StompClientLib!) {
-        print("Socket is connected successfully!")
-    }
-    
+
     func serverDidSendReceipt(
         client: StompClientLib!,
         withReceiptId receiptId: String
     ) {
-        print("Receipt : \(receiptId)")
+        print("Receipt: \(receiptId)")
     }
-    
+
     func serverDidSendError(
         client: StompClientLib!,
         withErrorMessage description: String,
         detailedErrorMessage message: String?
     ) {
-        print("Error : \(String(describing: message))")
-        // 연결에 실패하였습니다.
+        Log.error("Stomp Error: \(String(describing: message))")
     }
-    
+
     func serverDidSendPing() {
         print("Server Ping")
     }
